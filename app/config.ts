973f341c0540c20142f16525343715ebad82c8ee
Peter Beverloo <pbeverloo@gmail.com>
2023-05-18 22:33:26 +0100

    Reduce the number of lines on the copyright messages

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,5 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
 // Default title that should be shown on pages. Can be overridden by individual pages.
 export const kDefaultPageTitle = 'AnimeCon Volunteer Manager';
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
-// Use of this source code is governed by a MIT license that can be
-// found in the LICENSE file.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
 import { type Metadata } from 'next';
 
