57a0c62c2ba33858c250e7559a59504ffb238323
Peter Beverloo <pbeverloo@gmail.com>
2023-06-20 22:47:05 +0100

    Make Markdown content look half decent

--- app/components/Markdown.tsx
+++ app/components/Markdown.tsx
@@ -3,10 +3,66 @@
 
 'use client';
 
-import Link, { type LinkProps } from 'next/link';
+import Link from 'next/link';
 import { MuiMarkdown, defaultOverrides } from 'mui-markdown';
 
+import type { SxProps, Theme } from '@mui/system';
 import Box, { BoxProps } from '@mui/material/Box';
+import { default as MuiLink, LinkProps } from '@mui/material/Link';
+import Typography, { TypographyProps } from '@mui/material/Typography';
+import { lighten } from '@mui/material/styles';
+
+/**
+ * Manual styles that apply to the <Markdown> client component.
+ */
+const kStyles: { [key: string]: SxProps<Theme> } = {
+    root: {
+        '& blockquote': {
+            backgroundColor: theme => lighten(theme.palette.error.main, .8),
+            border: theme => '1px solid ' + theme.palette.error.main,
+            borderRadius: theme => theme.shape.borderRadius,
+
+            marginX: 0,
+            marginY: 2,
+            paddingX: 2,
+            paddingY: 0,
+
+            '& p': {
+                margin: 1,
+            }
+        },
+
+        '& p': { marginBottom: 2 },
+
+        '& h5': { /* none yet */ },
+        '& h6': { fontWeight: 800 },
+
+        '& h2, h3': { marginBottom: 0 },
+
+        '& h5 + p, h5 + ul': { marginTop: 0 },
+        '& h6 + p, h6 + ul': { marginTop: 0 },
+
+        '& li p': { margin: 0 },
+    },
+};
+
+/**
+ * Components for the different
+ */
+function Text(props: TypographyProps & { tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' }) {
+    switch (props.tag) {
+        case 'h1':
+        case 'h2':
+            return <Typography {...props} variant="h5" />
+        case 'h3':
+        case 'h4':
+        case 'h5':
+        case 'h6':
+            return <Typography {...props} variant="h6" />
+    }
+
+    return <Typography {...props} />
+}
 
 /**
  * Markdown replacement for both the native HTML <a> anchor element, as well as the Material UI
@@ -17,10 +73,10 @@ function LinkComponent(props: LinkProps & { baseUrl?: string, children?: React.R
         const resolvedUrl = new URL(props.href, document.location.origin + props.baseUrl);
         const resolvedHref = resolvedUrl.toString();
 
-        return <Link href={resolvedHref}>{props.children}</Link>;
+        return <MuiLink component={Link} href={resolvedHref}>{props.children}</MuiLink>;
     }
 
-    return <Link {...props} />;
+    return <MuiLink component={Link} {...props} />;
 }
 
 /**
@@ -47,11 +103,20 @@ export function Markdown(props: MarkdownProps) {
 
     return (
         <Box {...boxProps}>
-            <MuiMarkdown overrides={{
-                ...defaultOverrides,
-                a: { component: LinkComponent, props: { baseUrl } } }}>
-                {children}
-            </MuiMarkdown>
+            <Box sx={kStyles.root}>
+                <MuiMarkdown overrides={{
+                    ...defaultOverrides,
+                    a: { component: LinkComponent, props: { baseUrl } },
+                    h1: { component: Text, props: { tag: 'h1' } },
+                    h2: { component: Text, props: { tag: 'h2' } },
+                    h3: { component: Text, props: { tag: 'h3' } },
+                    h4: { component: Text, props: { tag: 'h4' } },
+                    h5: { component: Text, props: { tag: 'h5' } },
+                    h6: { component: Text, props: { tag: 'h6' } },
+                    p: { component: Text, props: { tag: 'p' } } }}>
+                    {children}
+                </MuiMarkdown>
+            </Box>
         </Box>
     );
 }
