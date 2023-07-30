652262eda9d1bbfa5a3851d28cb8ae6ff40c9ba3
Peter Beverloo <pbeverloo@gmail.com>
2023-07-30 17:22:10 +0100

    Contents for the event application page

--- app/registration/[[...path]]/ApplicationPage.tsx
+++ app/registration/[[...path]]/ApplicationPage.tsx
@@ -3,11 +3,17 @@
 
 'use client';
 
-import { useCallback, useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
+
+import {
+    type FieldValues, CheckboxElement, FormContainer, SelectElement,
+    TextFieldElement } from 'react-hook-form-mui';
 
 import type { SxProps, Theme } from '@mui/system';
 import Box from '@mui/material/Box';
 import FaceIcon from '@mui/icons-material/Face';
+import Grid from '@mui/material/Unstable_Grid2';
+import LoadingButton from '@mui/lab/LoadingButton';
 import MuiAvatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
@@ -20,6 +26,58 @@ import { type UserData } from '@app/lib/auth/UserData';
 import { AuthenticationContext } from '../AuthenticationContext';
 import { Avatar } from '@components/Avatar';
 import { Markdown } from '@components/Markdown';
+
+/**
+ * Valid options for the number of hours volunteers are willing to work. When updating an ID, make
+ * sure that `kDefaultValues` is updated as well.
+ */
+const kServiceHoursOptions = [
+    { id: '12', label: 'Up to 12 hours' },
+    { id: '16', label: '12–16 hours' },
+    { id: '20', label: '16–20 hours' },
+    { id: '24', label: 'More than 20 hours' },
+];
+
+/**
+ * Valid options for the timing of shifts a volunteer could be issued. When updating an ID, make
+ * sure that `kDefaultValues` is updated as well.
+ */
+const kServiceTimingOption = [
+    { id: '8-20', label: 'Early (08:00–20:00)' },
+    { id: '10-0', label: 'Regular (10:00–00:00)' },
+    { id: '14-3', label: 'Late (14:00–03:00)' },
+];
+
+/**
+ * Valid options for the t-shirt fit select field.
+ */
+const kTShirtFitOptions = [
+    { id: 'Regular', label: 'Regular' },
+    { id: 'Girly', label: 'Girly' },
+];
+
+/**
+ * Valid options for the t-shirt size select field.
+ */
+const kTShirtSizeOptions = [
+    { id: 'XS', label: 'XS' },
+    { id: 'S', label: 'Small' },
+    { id: 'M', label: 'Medium' },
+    { id: 'L', label: 'Large' },
+    { id: 'XL', label: 'XL' },
+    { id: 'XXL', label: 'XXL' },
+    { id: '3XL', label: '3XL' },
+    { id: '4XL', label: '4XL' },
+];
+
+/**
+ * Default values we pre-fill for incoming applications. The vast majority of volunteers choose
+ * these options anyway, and this allows more emphasis on adjusted values.
+ */
+const kDefaultValues = {
+    serviceHours: '16',
+    serviceTiming: '10-0',
+};
 
 /**
  * Manual styles that apply to the <ApplicationPage> client component.
@@ -81,19 +139,25 @@ export interface ApplicationPageProps {
  * A whole bunch of checks will have to be done in order to verify that they can.
  */
 export function ApplicationPage(props: ApplicationPageProps) {
-    const { user } = props;
+    const { event, user } = props;
 
     const authenticationContext = useContext(AuthenticationContext);
     const requestAuthenticationFlow = useCallback(() => {
         authenticationContext.requestAuthenticationFlow();
     }, [ authenticationContext ])
 
+    const [ loading, setLoading ] = useState<boolean>(false);
+
     // TODO: State - signed out
     // TODO: State - signed in but unregistered
     // TODO: State - signed in and registered
 
+    const availabilityLabel = `Yes, I will be fully available during ${event.shortName}`;
+    const creditsLabel = `Yes, I'd like to be included in the ${event.shortName} credit reel`;
+    const socialsLabel = 'Yes, I would like to join the private Discord and WhatsApp groups';
+
     return (
-        <>
+        <FormContainer defaultValues={kDefaultValues}>
             <Box sx={{ p: 2 }}>
                 {props.content && <Markdown sx={{ pb: 2 }}>{props.content.markdown}</Markdown> }
 
@@ -128,15 +192,47 @@ export function ApplicationPage(props: ApplicationPageProps) {
                 </Box>
 
                 <Typography variant="h6">
-                    Participation
+                    Your participation
                 </Typography>
-                { /* TODO */ }
+                <Grid container spacing={2} sx={{ pt: 1 }}>
+                    <Grid xs={6}>
+                        <SelectElement name="tshirtSize" label="T-shirt size" required
+                                       options={kTShirtSizeOptions} fullWidth size="small" />
+                    </Grid>
+                    <Grid xs={6}>
+                        <SelectElement name="tshirtFit" label="T-shirt fit" required
+                                       options={kTShirtFitOptions} fullWidth size="small" />
+                    </Grid>
 
-                <Typography variant="h6">
-                    Paperwork
+                    <Grid xs={6}>
+                        <SelectElement name="serviceHours" label="Number of shifts" required
+                                       options={kServiceHoursOptions} fullWidth size="small" />
+                    </Grid>
+                    <Grid xs={6}>
+                        <SelectElement name="serviceTiming" label="Timing of shifts" required
+                                       options={kServiceTimingOption} fullWidth size="small" />
+                    </Grid>
+
+                    <Grid xs={12}>
+                        <TextFieldElement name="preferences" label="Do you have any preferences?"
+                                          fullWidth size="small" />
+                    </Grid>
+                </Grid>
+
+                <Typography variant="h6" sx={{ pt: 2 }}>
+                    Some more questions…
                 </Typography>
-                { /* TODO */ }
+                <Stack>
+                    <CheckboxElement name="availability" size="small" label={availabilityLabel} />
+                    <CheckboxElement name="credits" size="small" label={creditsLabel} />
+                    <CheckboxElement name="socials" size="small" label={socialsLabel} />
+                </Stack>
+
+                <LoadingButton loading={loading} type="submit" variant="contained" sx={{ mt: 2 }}>
+                    Submit application
+                </LoadingButton>
+
             </Box>
-        </>
+        </FormContainer>
     );
 }
