cda0be1dd06003202790fd28d6ad19c0e55f7020
Peter Beverloo <pbeverloo@gmail.com>
2023-08-07 22:31:50 +0100

    Open a dropdown menu when clicking on Events

--- app/admin/AdminHeader.tsx
+++ app/admin/AdminHeader.tsx
@@ -4,21 +4,26 @@
 'use client';
 
 import Link from 'next/link';
+import { useState } from 'react';
 
 import type { SxProps, Theme } from '@mui/system';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import GridViewIcon from '@mui/icons-material/GridView';
-import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 import Paper from '@mui/material/Paper';
-import PersonIcon from '@mui/icons-material/Person';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
 import Stack from '@mui/material/Stack';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
 import Typography from '@mui/material/Typography';
 
 import type { UserData } from '@app/lib/auth/UserData';
 import { Avatar } from '@app/components/Avatar';
+import { Privilege, can } from '@app/lib/auth/Privileges';
 
 /**
  * Custom styles applied to the <AdminHeader> & related components.
@@ -50,6 +55,25 @@ export interface AdminHeaderProps {
 export function AdminHeader(props: AdminHeaderProps) {
     const { user } = props;
 
+    const [ anchorElement, setAnchorElement ] = useState<HTMLElement | null>(/* closed= */ null);
+
+    function requestMenuOpen(event: React.MouseEvent<HTMLElement>) {
+        setAnchorElement(event.currentTarget);
+    }
+
+    function requestMenuClose() {
+        setAnchorElement(/* closed= */ null);
+    }
+
+    // TODO: Select appropriate events for the `user` from the database, and pass them in to the
+    // <AdminHeader> component as a prop.
+    const events = [
+        { label: 'AnimeCon 2024', done: false, url: '/admin/events/2024' },
+        { label: 'AnimeCon 2023', done: true, url: '/admin/events/2023' },
+        { label: 'AnimeCon 2022: Classic', done: true, url: '/admin/events/2022-classic' },
+        { label: 'AnimeCon 2022', done: true, url: '/admin/events/2022' },
+    ];
+
     return (
         <Paper>
             <Stack direction="row" justifyContent="space-between" alignItems="center"
@@ -74,9 +98,39 @@ export function AdminHeader(props: AdminHeaderProps) {
                     Dashboard
                 </Button>
 
-                <Button endIcon={ <ExpandMoreIcon /> } variant="text" color="inherit">
+                <Button endIcon={ <ExpandMoreIcon /> } variant="text" color="inherit"
+                        onClick={requestMenuOpen}>
                     Events
                 </Button>
+
+                <Menu anchorEl={anchorElement} variant="menu"
+                      open={!!anchorElement}
+                      onClose={requestMenuClose}>
+
+                    { events.map((event, index) =>
+                        <MenuItem key={index} component={Link} href={event.url}>
+                            <ListItemIcon>
+                                { event.done && <TaskAltIcon color="disabled" fontSize="small" /> }
+                                { !event.done &&
+                                    <PlayCircleOutlineIcon color="success" fontSize="small" /> }
+                            </ListItemIcon>
+                            <ListItemText>
+                                {event.label}
+                            </ListItemText>
+                        </MenuItem> )}
+
+                    { can(user, Privilege.Administrator) && <Divider /> }
+                    { can(user, Privilege.Administrator) &&
+                        <MenuItem component={Link} href="/admin/events">
+                            <ListItemIcon>
+                                <SettingsIcon fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText>
+                                Manage events
+                            </ListItemText>
+                        </MenuItem> }
+
+                </Menu>
 
                 <Button component={Link} href="/admin/volunteers" variant="text" color="inherit">
                     Volunteers
