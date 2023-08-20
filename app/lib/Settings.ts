f4a848e5df627196b8117586708e038c8af3e554
Peter Beverloo <pbeverloo@gmail.com>
2023-08-20 22:45:42 +0100

    Add machinery to track settings in the app

---
+++ app/lib/Settings.ts
@@ -0,0 +1,108 @@
+// Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+import db, { tSettings } from '@lib/database';
+
+/**
+ * Represents the settings that can be stored and retrieved in the Volunteer Manager. These are all
+ * stored in the database, but typing is done exclusively client-side.
+ */
+type SettingsMap = {
+    // ---------------------------------------------------------------------------------------------
+    // Integration settings
+    // ---------------------------------------------------------------------------------------------
+
+    // Google:
+    'integration-google-credentials': string;
+    'integration-google-location': string;
+    'integration-google-project-id': string;
+
+    // Google Vertex AI:
+    'integration-vertex-model': 'text-bison' | 'text-bison@001';
+    'integration-vertex-temperature': number;
+    'integration-vertex-token-limit': number;
+    'integration-vertex-top-k': number;
+    'integration-vertex-top-p': number;
+};
+
+/**
+ * Reads the setting with the given `setting`, or `undefined` when it cannot be loaded. This
+ * function will end up issuing a database call.
+ */
+export async function readSetting<T extends keyof SettingsMap>(setting: T)
+    : Promise<SettingsMap[T] | undefined>
+{
+    return (await readSettings([ setting ]))[setting];
+}
+
+/**
+ * Reads the settings whose names are included in the given `settings`. An object will be returned
+ * with the setting values, or `undefined` when they cannot be loaded. This function will end up
+ * issuing a database call.
+ */
+export async function readSettings<T extends keyof SettingsMap>(settings: T[])
+    : Promise<{ [k in T]: SettingsMap[k] | undefined }>
+{
+    const storedValues = await db.selectFrom(tSettings)
+        .where(tSettings.settingName.in(settings))
+        .select({
+            name: tSettings.settingName,
+            value: tSettings.settingValue,
+        })
+        .executeSelectMany();
+
+    const result: { [k in T]: SettingsMap[k] | undefined } = { /* empty */ } as any;
+    for (const { name, value } of storedValues)
+        result[name as keyof typeof result] = JSON.parse(value);
+
+    return result;
+}
+
+/**
+ * Writes the setting with the given `setting` to the database, to be associated with the given
+ * `value` (which may be `undefined`). This function will end up issuing a database call.
+ */
+export async function writeSetting<T extends keyof SettingsMap>(setting: T, value?: SettingsMap[T])
+    : Promise<void>
+{
+    await writeSettings({ [setting]: value } as any);
+}
+
+/**
+ * Writes the given `settings` to the database. Each key in `settings` must be a valid setting with
+ * the appropriate type, or `undefined`. This function works by deleting all keys from the database
+ * and then creating new rows for the settings with values, all within a transaction.
+ */
+export async function writeSettings<T extends keyof SettingsMap>(
+    settings: { [k in T]: SettingsMap[k] | undefined }) : Promise<void>
+{
+    const dbInstance = db;
+    await dbInstance.transaction(async () => {
+        const keysToDelete: T[] = [];
+        const keysToInsert: { settingName: string, settingValue: string }[] = [];
+
+        for (const [ setting, value ] of Object.entries(settings)) {
+            keysToDelete.push(setting as T);
+
+            if (typeof value !== 'undefined')
+                keysToInsert.push({ settingName: setting, settingValue: JSON.stringify(value) });
+        }
+
+        console.log(keysToDelete);
+        console.log(keysToInsert);
+
+        // Delete stored settings that no longer should have a value.
+        if (keysToDelete.length) {
+            await dbInstance.deleteFrom(tSettings)
+                .where(tSettings.settingName.in(keysToDelete))
+                .executeDelete(/* min= */ 0, /* max= */ keysToDelete.length);
+        }
+
+        // Insert new rows for stored settings that do have a value.
+        if (keysToInsert.length) {
+            await dbInstance.insertInto(tSettings)
+                .values(keysToInsert)
+                .executeInsert(/* min= */ 0, /* max= */ keysToInsert.length);
+        }
+    });
+}
---
+++ app/lib/database/scheme/SettingsTable.ts
@@ -0,0 +1,20 @@
+/* eslint-disable quotes, max-len */
+/**
+ * DO NOT EDIT:
+ *
+ * This file has been auto-generated from database schema using ts-sql-codegen.
+ * Any changes will be overwritten.
+ */
+import { Table } from "ts-sql-query/Table";
+import type { DBConnection } from "../Connection";
+
+export class SettingsTable extends Table<DBConnection, 'SettingsTable'> {
+    settingName = this.column('setting_name', 'string');
+    settingValue = this.column('setting_value', 'string');
+
+    constructor() {
+        super('settings');
+    }
+}
+
+
