d691a79db5ce4650bd56b06d86e62771bec67622
Peter Beverloo <pbeverloo@gmail.com>
2023-09-09 22:39:03 +0100

    Add the new HotelsPreferences table definition

---
+++ app/lib/database/scheme/HotelsPreferencesTable.ts
@@ -0,0 +1,29 @@
+// @ts-nocheck
+/* eslint-disable quotes, max-len */
+/**
+ * DO NOT EDIT:
+ *
+ * This file has been auto-generated from database schema using ts-sql-codegen.
+ * Any changes will be overwritten.
+ */
+import { Table } from "ts-sql-query/Table";
+import type { DBConnection } from "../Connection";
+
+export class HotelsPreferencesTable extends Table<DBConnection, 'HotelsPreferencesTable'> {
+    userId = this.column('user_id', 'int');
+    eventId = this.column('event_id', 'int');
+    teamId = this.column('team_id', 'int');
+    hotelId = this.optionalColumnWithDefaultValue('hotel_id', 'int');
+    hotelDateCheckIn = this.optionalColumnWithDefaultValue('hotel_date_check_in', 'localDateTime');
+    hotelDateCheckOut = this.optionalColumnWithDefaultValue('hotel_date_check_out', 'localDateTime');
+    hotelSharingPeople = this.optionalColumnWithDefaultValue('hotel_sharing_people', 'int');
+    hotelSharingPreferences = this.optionalColumnWithDefaultValue('hotel_sharing_preferences', 'string');
+    hotelLicensePlate = this.optionalColumnWithDefaultValue('hotel_license_plate', 'string');
+    hotelPreferencesUpdated = this.columnWithDefaultValue('hotel_preferences_updated', 'localDateTime');
+
+    constructor() {
+        super('hotels_preferences');
+    }
+}
+
+
