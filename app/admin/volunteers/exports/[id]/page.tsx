bdeb3530fb1ec2e4c0dabeb404bbf623d08060be
Peter Beverloo <pbeverloo@gmail.com>
2023-10-21 23:15:49 +0100

    Show the metadata of a data export

--- app/admin/volunteers/exports/[id]/page.tsx
+++ app/admin/volunteers/exports/[id]/page.tsx
@@ -2,13 +2,17 @@
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
 import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 
+import Alert from '@mui/material/Alert';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 import type { NextRouterParams } from '@lib/NextRouterParams';
+import { ExportMetadata } from './ExportMetadata';
 import { Privilege } from '@lib/auth/Privileges';
 import { requireAuthenticationContext } from '@lib/auth/AuthenticationContext';
+import db, { tEvents, tExportsLogs, tExports, tUsers } from '@lib/database';
 
 /**
  * The <VolunteersExportDetailsPage> component is the page that displays detailed information about
@@ -20,14 +24,47 @@ export default async function VolunteersExportDetailsPage(props: NextRouterParam
         privilege: Privilege.VolunteerDataExports,
     });
 
+    const exportsLogsJoin = tExportsLogs.forUseInLeftJoin();
+
+    const dbInstance = db;
+    const data = await dbInstance.selectFrom(tExports)
+        .innerJoin(tEvents)
+            .on(tEvents.eventId.equals(tExports.exportEventId))
+        .innerJoin(tUsers)
+            .on(tUsers.userId.equals(tExports.exportCreatedUserId))
+        .leftJoin(exportsLogsJoin)
+            .on(exportsLogsJoin.exportId.equals(tExports.exportId))
+        .where(tExports.exportId.equals(parseInt(props.params.id, 10)))
+        .select({
+            date: tExports.exportCreatedDate,
+            slug: tExports.exportSlug,
+            type: tExports.exportType,
+            eventName: tEvents.eventShortName,
+            expirationDate: tExports.exportExpirationDate,
+            expirationViews: tExports.exportExpirationViews,
+            justification: tExports.exportJustification,
+            userId: tExports.exportCreatedUserId,
+            userName: tUsers.firstName.concat(' ').concat(tUsers.lastName),
+            views: dbInstance.count(exportsLogsJoin.accessDate)
+        })
+        .groupBy(tExports.exportId)
+        .executeSelectNoneOrOne();
+
+    if (!data)
+        notFound();
+
     return (
         <>
             <Paper sx={{ p: 2 }}>
                 <Typography variant="h5" sx={{ mb: 1 }}>
                     Exports
                 </Typography>
+                <Alert severity="info" sx={{ mb: 1 }}>
+                    Data exports are immutable—instead, create a new export and <em>delete</em> this
+                    export if necessary.
+                </Alert>
+                <ExportMetadata metadata={data} />
             </Paper>
-            { /* TODO: Export metadata */ }
             { /* TODO: Export access log */ }
         </>
     )
