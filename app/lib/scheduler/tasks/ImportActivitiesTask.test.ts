23fe12c9e5a89bc3f47a821fbf781e55b653830a
Peter Beverloo <pbeverloo@gmail.com>
2023-12-22 12:02:21 +0000

    Have the `ImportActivitiesTask` scale its execution time

--- app/lib/scheduler/tasks/ImportActivitiesTask.test.ts
+++ app/lib/scheduler/tasks/ImportActivitiesTask.test.ts
@@ -3,6 +3,7 @@
 
 import { ImportActivitiesTask } from './ImportActivitiesTask';
 import { TaskContext } from '../TaskContext';
+import { dayjs } from '@lib/DateTime';
 import { useMockConnection } from '@lib/database/Connection';
 
 describe('ImportActivitiesTask', () => {
@@ -13,12 +14,13 @@ describe('ImportActivitiesTask', () => {
         festivalId: number;
     }
 
-    function createImportActivitiesTaskForFestival(festival?: FestivalOptions, params?: unknown) {
-        const context = TaskContext.forEphemeralTask('ImportActivitiesTask', params);
+    function createImportActivitiesTaskForFestival(festival?: FestivalOptions, skipDb?: boolean) {
+        const context = TaskContext.forEphemeralTask('ImportActivitiesTask', { /* no params */ });
         const task = new ImportActivitiesTask(context);
-        expect(task.isSimpleTask()).toBeTrue();  // validate |params| if this fails
+        expect(task.isSimpleTask()).toBeTrue();  // validate params if this fails
 
-        mockConnection.expect('selectOneRow', () => festival);
+        if (!skipDb)
+            mockConnection.expect('selectOneRow', () => festival);
 
         return task;
     }
@@ -34,6 +36,43 @@ describe('ImportActivitiesTask', () => {
         expect(task.log.entries).toHaveLength(1);
         expect(task.log.entries[0].message).toInclude('no upcoming events');
 
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportActivitiesTask.kIntervalNoUpcomingEvents);
+    });
+
+    it('should scale the task interval based on duration until the festival', async () => {
+        const task = createImportActivitiesTaskForFestival(
+            /* no festival= */ undefined, /* skipDb= */ true);
+
+        expect(task.contextForTesting.intervalMsForTesting).toBeUndefined();
+
+        // (1) More than four months
+        task.updateTaskIntervalForFestivalDate(dayjs().add(5, 'months').toDate());
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportActivitiesTask.kIntervalDefault);
+
+        // (2) More than two months
+        task.updateTaskIntervalForFestivalDate(dayjs().add(3, 'months').toDate());
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportActivitiesTask.kIntervalFinalFourMonths);
+
+        // (3) More than one month
+        task.updateTaskIntervalForFestivalDate(dayjs().add(35, 'days').toDate());
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportActivitiesTask.kIntervalFinalTwoMonths);
+
+        // (4) More than two weeks
+        task.updateTaskIntervalForFestivalDate(dayjs().add(15, 'days').toDate());
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportActivitiesTask.kIntervalFinalMonth);
+
+        // (5) Less than (or equal to) two weeks
+        task.updateTaskIntervalForFestivalDate(dayjs().add(7, 'days').toDate());
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportActivitiesTask.kIntervalFinalTwoWeeks);
+
+        // (6) Event in the past (possible due to timezone stuff)
+        task.updateTaskIntervalForFestivalDate(dayjs().subtract(1, 'day').toDate());
         expect(task.contextForTesting.intervalMsForTesting).toBe(
             ImportActivitiesTask.kIntervalNoUpcomingEvents);
     });
--- app/lib/scheduler/tasks/ImportActivitiesTask.ts
+++ app/lib/scheduler/tasks/ImportActivitiesTask.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
 import { Task } from '../Task';
+import { dayjs } from '@lib/DateTime';
 import db, { tEvents } from '@lib/database';
 
 /**
@@ -10,9 +11,34 @@ import db, { tEvents } from '@lib/database';
  */
 export class ImportActivitiesTask extends Task {
     /**
+     * The default interval for the import task, when no precise granularity can be decided upon.
+     */
+    static readonly kIntervalDefault = /* 1 week= */ 7 * 86400 * 1000;
+
+    /**
+     * Interval, in milliseconds, for this task to execute at in the final four months.
+     */
+    static readonly kIntervalFinalFourMonths = /* 3 days= */ 3 * 86400 * 1000;
+
+    /**
+     * Interval, in milliseconds, for this task to execute at in the final two months.
+     */
+    static readonly kIntervalFinalTwoMonths = /* 1 day= */ 1 * 86400 * 1000;
+
+    /**
+     * Interval, in milliseconds, for this task to execute at in the final month.
+     */
+    static readonly kIntervalFinalMonth = /* 6 hours= */ 6 * 3600 * 1000;
+
+    /**
+     * Interval, in milliseconds, for this task to execute at in the final two weeks.
+     */
+    static readonly kIntervalFinalTwoWeeks = /* 15 minutes= */ 900 * 1000;
+
+    /**
      * Maximum interval that will be applied when there are no upcoming events, in milliseconds.
      */
-    static readonly kIntervalNoUpcomingEvents = /* 1 day= */ 86400000;
+    static readonly kIntervalNoUpcomingEvents = /* 1 week= */ 7 * 86400 * 1000;
 
     // ---------------------------------------------------------------------------------------------
 
@@ -26,14 +52,35 @@ export class ImportActivitiesTask extends Task {
 
         const { festivalEndTime, festivalId } = upcomingEvent;
 
+        // The interval of this task depends on how close we are to the festival. The following
+        // function will scale the task interval appropriate to this.
+        this.updateTaskIntervalForFestivalDate(festivalEndTime);
+
         // TODO: Acquire all festival activities from the AnimeCon API.
         // TODO: Synchronise the API's activities with our database state.
 
-        // TODO: Scale the interval of this task based on how far into the future the festival is.
         return true;
     }
 
     // ---------------------------------------------------------------------------------------------
+
+    updateTaskIntervalForFestivalDate(endTime: Date): void {
+        const differenceInDays = dayjs(endTime).diff(dayjs(), 'days', /* float= */ true);
+
+        let intervalMs = ImportActivitiesTask.kIntervalDefault;
+        if (differenceInDays < 0)
+            intervalMs = ImportActivitiesTask.kIntervalNoUpcomingEvents;
+        else if (differenceInDays < 14)
+            intervalMs = ImportActivitiesTask.kIntervalFinalTwoWeeks;
+        else if (differenceInDays < 30.25)
+            intervalMs = ImportActivitiesTask.kIntervalFinalMonth;
+        else if (differenceInDays < 60.50)
+            intervalMs = ImportActivitiesTask.kIntervalFinalTwoMonths;
+        else if (differenceInDays < 121)
+            intervalMs = ImportActivitiesTask.kIntervalFinalFourMonths;
+
+        this.setIntervalForRepeatingTask(intervalMs);
+    }
 
     private async selectCurrentOrUpcomingEventWithFestivalId() {
         return await db.selectFrom(tEvents)
