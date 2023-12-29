ddf22197f47dc1057feba5117c5fb2967f30c74f
Peter Beverloo <pbeverloo@gmail.com>
2023-12-29 17:33:34 +0000

    Fill in expectations based on festival times, and indicated shift window

--- app/registration/[slug]/application/availability/page.tsx
+++ app/registration/[slug]/application/availability/page.tsx
@@ -8,7 +8,7 @@ import { default as MuiLink } from '@mui/material/Link';
 import Box from '@mui/material/Box';
 
 import type { NextRouterParams } from '@lib/NextRouterParams';
-import { AvailabilityExpectations, type AvailabilityDayInfo } from './AvailabilityExpectations';
+import { AvailabilityExpectations, type AvailabilityDayInfo, type AvailabilityExpectation } from './AvailabilityExpectations';
 import { AvailabilityPreferences } from './AvailabilityPreferences';
 import { Markdown } from '@components/Markdown';
 import { Privilege, can } from '@lib/auth/Privileges';
@@ -43,17 +43,90 @@ export default async function EventApplicationAvailabilityPage(props: NextRouter
     // Section: Availability
     // ---------------------------------------------------------------------------------------------
 
-    const startDate = dayjs(event.startTime).startOf('day');
-    const endDate = dayjs(event.endTime).endOf('day');
+    const startDateTime = dayjs(event.startTime);
+    const startDate = startDateTime.startOf('day');
+
+    const endDateTime = dayjs(event.endTime);
+    const endDate = endDateTime.endOf('day');
+
+    let serviceTimingStart: number | undefined;
+    let serviceTimingEnd: number | undefined;
+
+    if (!!registration.availability.serviceTiming) {
+        [ serviceTimingStart, serviceTimingEnd ] =
+            registration.availability.serviceTiming.split('-').map(v => parseInt(v, 10));
+    }
 
     const expectations: AvailabilityDayInfo[] = [];
     for (let date = startDate; date.isBefore(endDate); date = date.add(1, 'day')) {
-        // TODO: Consider their preferred timing.
-        // TODO: Consider the events they would like to attend.
-
         expectations.push({
             label: date.format('dddd, MMMM D'),
-            expectations: [ ...Array(/* hours= */ 24) ].map(v => 'unavailable'),
+            expectations: [ ...Array(/* hours= */ 24) ].map((_, hour) => {
+                const hourlyDateTime = dayjs(date).add(hour, 'hours');
+                let decidedStatus: AvailabilityExpectation = 'available';
+
+                // TODO: Consider exceptions "approved" by volunteering leads
+                // TODO: Consider the events the volunteer would like to attend
+
+                // Consider the window in which the volunteer indicated they want to help out with
+                // shifts. We'll add some grace, but otherwise will roster them out at other times.
+                if (serviceTimingStart !== undefined && serviceTimingEnd !== undefined) {
+                    if (serviceTimingEnd > serviceTimingStart) {
+                        // Case (1): The volunteer's shifts will start and end on the same day.
+                        const hoursUntilWindowStart = serviceTimingStart - hour;
+                        const hoursUntilWindowEnd = serviceTimingEnd - hour;
+
+                        if (hoursUntilWindowStart > 1)
+                            decidedStatus = 'unavailable';
+                        else if (hoursUntilWindowStart > 0)
+                            decidedStatus = 'avoid';
+
+                        if (hoursUntilWindowEnd === 0)
+                            decidedStatus = 'avoid';
+                        else if (hoursUntilWindowEnd < 0)
+                            decidedStatus = 'unavailable';
+
+                    } else {
+                        // Case (2): The volunteer's shifts will start and end on separate days.
+                        const hoursUntilWindowStart = serviceTimingStart - hour;
+
+                        if (!date.isSame(startDate, 'day')) {
+                            if (hour === serviceTimingEnd)
+                                decidedStatus = 'avoid';
+                            else if (hour > serviceTimingEnd && hoursUntilWindowStart > 2)
+                                decidedStatus = 'unavailable';
+                        }
+
+                        if (hour > serviceTimingEnd) {
+                            if (hoursUntilWindowStart > 1)
+                                decidedStatus = 'unavailable';
+                            else if (hoursUntilWindowStart > 0)
+                                decidedStatus = 'avoid';
+                        }
+                    }
+                }
+
+                // We won't schedule shifts (well) before the festival's opening time without having
+                // discussed this with the volunteer.
+                if (date.isSame(startDate, 'day')) {
+                    const hoursUntilOpening = startDateTime.diff(hourlyDateTime, 'hours');
+                    if (hoursUntilOpening > 3)
+                        return 'unavailable';
+                    else if (hoursUntilOpening > 1)
+                        return 'avoid';  // always invite volunteers to the briefing
+                }
+
+                // Similarly, we won't schedule shifts after the festival has finished. Folks are
+                // welcome to stick around, but we won't count on it.
+                if (date.isSame(endDate, 'day')) {
+                    const hoursUntilClosure = endDateTime.diff(hourlyDateTime, 'hours');
+                    if (hoursUntilClosure <= 0)
+                        return 'unavailable';
+                }
+
+                // If all else fails, the volunteer will be available.
+                return decidedStatus;
+            }),
         });
     }
 
