cead5f9f8f5d7e1e8e8c4d136792e543576b0de9
Peter Beverloo <pbeverloo@gmail.com>
2024-01-08 15:59:37 +0000

    Add slightly nicer warnings to unsupported pages

--- app/admin/events/[slug]/program/activities/[id]/page.tsx
+++ app/admin/events/[slug]/program/activities/[id]/page.tsx
@@ -1,7 +1,7 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
 
 import type { NextRouterParams } from '@lib/NextRouterParams';
 import { verifyAccessAndFetchPageInfo } from '@app/admin/events/verifyAccessAndFetchPageInfo';
@@ -14,8 +14,9 @@ export default async function ProgramActivityPage(props: NextRouterParams<'slug'
     const { event } = await verifyAccessAndFetchPageInfo(props.params);
 
     return (
-        <Typography variant="body2">
-            ProgramActivityPage ({props.params.slug}, {props.params.id})
-        </Typography>
+        <Alert severity="warning" sx={{ m: 2 }}>
+            This page has not been implemented yet. (ProgramActivityPage, {props.params.slug},
+            {' '}{props.params.id})
+        </Alert>
     );
 }
