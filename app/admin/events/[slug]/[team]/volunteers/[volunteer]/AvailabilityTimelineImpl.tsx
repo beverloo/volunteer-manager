8df2ae60a6989b1e674c008d914ddf4fe2fe9fac
Peter Beverloo <pbeverloo@gmail.com>
2024-01-24 19:05:36 +0000

    Display error messages as snackbars

--- app/admin/events/[slug]/[team]/volunteers/[volunteer]/AvailabilityTimelineImpl.tsx
+++ app/admin/events/[slug]/[team]/volunteers/[volunteer]/AvailabilityTimelineImpl.tsx
@@ -1,6 +1,13 @@
 // Copyright 2024 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
+'use client';
+
+import { useCallback, useState } from 'react';
+
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
+
 import type { AvailabilityTimeslot } from '@beverloo/volunteer-manager-timeline';
 import { AvailabilityTimeline } from '@beverloo/volunteer-manager-timeline';
 import { dayjs } from '@lib/DateTime';
@@ -58,11 +65,25 @@ export function AvailabilityTimelineImpl(props: AvailabilityTimelineImplProps) {
     const { min, max, onChange, readOnly, theme, timeslots, timezone } = props;
 
     // TODO: onDoubleClick
-    // TODO: onError
+
+    const [ errorOpen, setErrorOpen ] = useState<boolean>(false);
+
+    const handleErrorClose = useCallback(() => setErrorOpen(false), [ /* no deps */ ]);
+    const handleError = useCallback((action: 'create' | 'update', reason: 'overlap') => {
+        setErrorOpen(true);
+    }, [ /* no deps */ ]);
 
     return (
-        <AvailabilityTimeline dayjs={dayjs} min={min} max={max}
-                              dataTimezone="utc" displayTimezone={timezone} theme={theme}
-                              onChange={onChange} readOnly={readOnly} timeslots={timeslots} />
+        <>
+            <AvailabilityTimeline dayjs={dayjs} min={min} max={max}
+                                  dataTimezone="utc" displayTimezone={timezone} theme={theme}
+                                  onChange={onChange} onError={handleError}
+                                  readOnly={readOnly} timeslots={timeslots} />
+            <Snackbar autoHideDuration={3000} onClose={handleErrorClose} open={errorOpen}>
+                <Alert severity="error" variant="filled" sx={{ width: '100%' }}>
+                    Exceptions cannot overlap with each other
+                </Alert>
+            </Snackbar>
+        </>
     );
 }
