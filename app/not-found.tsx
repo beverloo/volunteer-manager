d6b6066a12444f7ea44773e260a9e9980f2fe948
Peter Beverloo <pbeverloo@gmail.com>
2024-01-31 19:28:09 +0000

    Add a pretty HTTP 404 error page

---
+++ app/not-found.tsx
@@ -0,0 +1,40 @@
+// Copyright 2024 Peter Beverloo & AnimeCon. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation'
+
+import { RegistrationContent } from './registration/RegistrationContent';
+import { RegistrationContentContainer } from '@app/registration/RegistrationContentContainer';
+import { RegistrationLayout } from './registration/RegistrationLayout';
+import { determineEnvironment } from '@lib/Environment';
+import { getAuthenticationContext } from '@lib/auth/AuthenticationContext';
+import { getStaticContent } from '@lib/Content';
+
+/**
+ * Root component shown when the requested page could not be found, or the signed in user does not
+ * have access to it. This page explains the situation to the user.
+ */
+export default async function NotFoundPage() {
+    const environment = await determineEnvironment();
+    if (!environment)
+        notFound();
+
+    const { user } = await getAuthenticationContext();
+
+    const content = await getStaticContent([ 'errors/404' ]);
+    if (!content)
+        notFound();
+
+    return (
+        <RegistrationLayout environment={environment}>
+            <RegistrationContentContainer title={content.title} user={user}>
+                <RegistrationContent content={content}/>
+            </RegistrationContentContainer>
+        </RegistrationLayout>
+    );
+}
+
+export const metadata: Metadata = {
+    title: 'Page not found | AnimeCon Volunteering Teams',
+};
