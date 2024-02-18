fedc0a70d50d05ad4e117d868a47b405dadc57b7
Peter Beverloo <pbeverloo@gmail.com>
2024-02-18 21:12:45 +0000

    Fix: order the training options by date

--- app/registration/[slug]/application/training/getTrainingOptions.ts
+++ app/registration/[slug]/application/training/getTrainingOptions.ts
@@ -20,6 +20,7 @@ export async function getTrainingOptions(eventId: number) {
             trainingStart: tTrainings.trainingStart,
             trainingEnd: tTrainings.trainingEnd,
         })
+        .orderBy('trainingStart', 'asc')
         .executeSelectMany();
 
     return trainings.map(training => {
