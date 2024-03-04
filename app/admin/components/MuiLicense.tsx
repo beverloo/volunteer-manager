c7eca5d35dd6083b9c74fef8d70e416d2c9f373c
Peter Beverloo <pbeverloo@gmail.com>
2024-03-04 19:00:51 +0000

    Make sure that the license key constant reaches client-side code

--- app/admin/components/MuiLicense.tsx
+++ app/admin/components/MuiLicense.tsx
@@ -9,8 +9,7 @@ import { LicenseInfo } from '@mui/x-license-pro';
  * Install the MUI License key. The key itself is specified as an environment variable. When no key
  * is passed, a watermark will be shown instead when MUI X Pro features are being used.
  */
-if (!!process.env.APP_MUI_LICENSE_KEY)
-    LicenseInfo.setLicenseKey(process.env.APP_MUI_LICENSE_KEY);
+LicenseInfo.setLicenseKey(process.env.NEXT_PUBLIC_MUI_LICENSE_KEY!);
 
 /**
  * The <MuiLicense> element does nothing, but has a side effect of initialising the `LicenseInfo`
