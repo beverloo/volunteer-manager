2ec4df3208f6da3a5e1d9f9380c13786b56bcec1
Peter Beverloo <pbeverloo@gmail.com>
2024-03-06 21:55:02 +0000

    Include the account name in "confirmed existence" log messages

--- app/lib/LogLoader.ts
+++ app/lib/LogLoader.ts
@@ -78,7 +78,9 @@ const kLogMessageFormatter: {
     [LogType.AccountIdentifyAccessCode]: 'Signed in using an access code',
     [LogType.AccountIdentifyPasskey]: 'Signed in using a passkey',
     [LogType.AccountIdentifyPassword]: 'Signed in using a password',
-    [LogType.AccountIdentityCheck]: 'Confirmed existence of an account',
+    [LogType.AccountIdentityCheck]: (source, target, { username }) => {
+        return `Confirmed existence of an account (${username})`;
+    },
     [LogType.AccountPasskeyCreate]: 'Created a passkey for their account',
     [LogType.AccountPasskeyDelete]: 'Deleted a passkey from their account',
     [LogType.AccountPasswordResetRequest]: 'Requested their password to be reset',
