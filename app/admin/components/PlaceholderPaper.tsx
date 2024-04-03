221e08fd4fedf17e9c0a5a2b9b0cce67954efa84
Peter Beverloo <pbeverloo@gmail.com>
2024-04-03 22:10:54 +0100

    Rewrite <PlaceholderPaper> to use `sx`, shaving off ~50ms

--- app/admin/components/PlaceholderPaper.tsx
+++ app/admin/components/PlaceholderPaper.tsx
@@ -3,21 +3,24 @@
 
 'use client';
 
-import Paper, { type PaperProps, paperClasses } from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+import Paper, { type PaperProps } from '@mui/material/Paper';
+import { useTheme } from '@mui/material/styles';
 
 /**
  * The <PlaceholderPaper> component can be used instead of a <Paper> component, although rather than
  * drawing a realistic paper it draws a dashed-border outlined version of one.
  */
-export const PlaceholderPaper = styled((props: Omit<PaperProps, 'elevation'>) => (
-    <Paper {...props} elevation={0}>
-        {props.children}
-    </Paper>
-))(({ theme }) => ({
-    [`&.${paperClasses.root}`]: {
-        border: '2px dashed rgba(0, 0, 0, .1)',
-        backgroundColor: theme.palette.mode === 'light' ? 'rgba(0, 0, 0, .05)'
-                                                        : 'rgba(255, 255, 255, .05)',
-    },
-}));
+export function PlaceholderPaper(props: Omit<PaperProps, 'elevation'>) {
+    const { sx, ...otherProps } = props;
+
+    const theme = useTheme();
+    return (
+        <Paper {...otherProps} elevation={0}
+               sx={{
+                   ...sx,
+                   border: '2px dashed rgba(0, 0, 0, .1)',
+                   backgroundColor: theme.palette.mode === 'light' ? 'rgba(0, 0, 0, .05)'
+                                                                   : 'rgba(255, 255, 255, .05)',
+               }} />
+    );
+}
