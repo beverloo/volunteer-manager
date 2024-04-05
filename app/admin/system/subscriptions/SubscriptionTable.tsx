d6dc05a8d26eb08d528434d509d814d7baf8c15f
Peter Beverloo <pbeverloo@gmail.com>
2024-04-05 23:43:07 +0100

    List all eligible volunteers + subscriptions in the table

--- app/admin/system/subscriptions/SubscriptionTable.tsx
+++ app/admin/system/subscriptions/SubscriptionTable.tsx
@@ -3,8 +3,37 @@
 
 'use client';
 
+import Link from 'next/link';
+
+import type { GridCellParams } from '@mui/x-data-grid-pro';
+import { default as MuiLink } from '@mui/material/Link';
+import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import Tooltip from '@mui/material/Tooltip';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 import type { SubscriptionsRowModel } from '@app/api/admin/subscriptions/[[...id]]/route';
 import { RemoteDataTable, type RemoteDataTableColumn } from '@app/admin/components/RemoteDataTable';
+import { SubscriptionType } from '@lib/database/Types';
+
+/**
+ * Human-readable descriptions of each of the subscriptions that can be created. Will be presented
+ * in the data table.
+ */
+const kSubscriptionDescriptions: { [k in SubscriptionType]: string } = {
+    [SubscriptionType.Application]: 'When a volunteer applies to help out in this team',
+    [SubscriptionType.Registration]: 'When a volunteer has created an account',
+};
+
+/**
+ * Returns whether the row indicated by the given `params` represents a user, as opposed to a
+ * subscription that could be created for said user.
+ */
+function isUserRow(params: GridCellParams<SubscriptionsRowModel, unknown>) {
+    return !params.row.id.includes('/');
+}
 
 /**
  * The <SubscriptionTable> component displays a MUI Data Grid that lists all individuals who are
@@ -13,12 +42,119 @@ import { RemoteDataTable, type RemoteDataTableColumn } from '@app/admin/componen
  */
 export function SubscriptionTable() {
     const columns: RemoteDataTableColumn<SubscriptionsRowModel>[] = [
+        {
+            field: 'name',
+            headerName: 'Subscription',
+            editable: false,
+            sortable: false,
+            flex: 1,
 
+            renderCell: params => {
+                if (isUserRow(params)) {
+                    return (
+                        <MuiLink component={Link} href={`/admin/volunteers/${params.row.id}`}>
+                            {params.value}
+                        </MuiLink>
+                    );
+                } else {
+                    return params.value;
+                }
+            },
+        },
+        {
+            field: 'description',
+            headerName: 'Description',
+            editable: false,
+            sortable: false,
+            flex: 2,
+
+            renderCell: params =>
+                params.value ||
+                    (!!params.row.type ? kSubscriptionDescriptions[params.row.type] : ''),
+        },
+        {
+            field: 'channelEmail',
+            headerName: /* empty= */ '',
+            headerAlign: 'center',
+            align: 'center',
+            editable: true,
+            sortable: false,
+            width: 50,
+
+            type: 'boolean',
+
+            renderHeader: () =>
+                <Tooltip title="Distribute using e-mail">
+                    <MailOutlineIcon color="primary" fontSize="small" />
+                </Tooltip>,
+
+            renderCell: params => {
+                if (isUserRow(params)) {
+                    return /* empty column= */ '';
+                } else if (!!params.value) {
+                    return <CheckCircleIcon fontSize="small" color="success" />;
+                } else {
+                    return <CancelIcon fontSize="small" color="error" />;
+                }
+            },
+        },
+        {
+            field: 'channelNotification',
+            headerName: /* empty= */ '',
+            headerAlign: 'center',
+            align: 'center',
+            editable: true,
+            sortable: false,
+            width: 50,
+
+            type: 'boolean',
+
+            renderHeader: () =>
+                <Tooltip title="Distribute using Web Push">
+                    <NotificationsNoneIcon color="primary" fontSize="small" />
+                </Tooltip>,
+
+            renderCell: params => {
+                if (isUserRow(params)) {
+                    return /* empty column= */ '';
+                } else if (!!params.value) {
+                    return <CheckCircleIcon fontSize="small" color="success" />;
+                } else {
+                    return <CancelIcon fontSize="small" color="error" />;
+                }
+            },
+        },
+        {
+            field: 'channelWhatsapp',
+            headerName: /* empty= */ '',
+            headerAlign: 'center',
+            align: 'center',
+            editable: true,
+            sortable: false,
+            width: 50,
+
+            type: 'boolean',
+
+            renderHeader: () =>
+                <Tooltip title="Distribute using WhatsApp">
+                    <WhatsAppIcon color="primary" fontSize="small" />
+                </Tooltip>,
+
+            renderCell: params => {
+                if (isUserRow(params)) {
+                    return /* empty column= */ '';
+                } else if (!!params.value) {
+                    return <CheckCircleIcon fontSize="small" color="success" />;
+                } else {
+                    return <CancelIcon fontSize="small" color="error" />;
+                }
+            },
+        }
     ];
 
     return (
         <RemoteDataTable columns={columns} endpoint="/api/admin/subscriptions"
-                         defaultSort={{ field: 'id', sort: 'asc' }} pageSize={100}
-                         disableFooter enableUpdate />
+                         defaultSort={{ field: 'name', sort: 'asc' }} pageSize={100}
+                         disableFooter enableUpdate isCellEditable={isUserRow} />
     );
 }
--- app/api/admin/subscriptions/[[...id]]/route.ts
+++ app/api/admin/subscriptions/[[...id]]/route.ts
@@ -4,20 +4,50 @@
 import { z } from 'zod';
 
 import { type DataTableEndpoints, createDataTableApi } from '../../../createDataTableApi';
-import { LogSeverity } from '@lib/database/Types';
+import { LogSeverity, SubscriptionType } from '@lib/database/Types';
 import { LogType, Log } from '@lib/Log';
 import { Privilege } from '@lib/auth/Privileges';
 import { executeAccessCheck } from '@lib/auth/AuthenticationContext';
-import db, { tSubscriptions, tUsers } from '@lib/database';
+import db, { tSubscriptions, tTeams, tUsers } from '@lib/database';
 
 /**
  * Row model for a subscription.
  */
 const kSubscriptionRowModel = z.object({
     /**
-     * Unique ID of the subscription, as it exists in the database.
+     * Unique ID of the subscription row, referring to its representation in the database.
      */
-    id: z.number(),
+    id: z.string(),
+
+    /**
+     * Name of the volunteer who can have subscriptions.
+     */
+    name: z.string(),
+
+    /**
+     * Optional description to render in the "description" column.
+     */
+    description: z.string().optional(),
+
+    /**
+     * Type of subscription that this row is dealing with.
+     */
+    type: z.nativeEnum(SubscriptionType).optional(),
+
+    /**
+     * Whether notifications for this subscription will be delivered by e-mail.
+     */
+    channelEmail: z.boolean().optional(),
+
+    /**
+     * Whether notifications for this subscription will be delivered as a Web Push Notification.
+     */
+    channelNotification: z.boolean().optional(),
+
+    /**
+     * Whether notifications for this subscription will be delivered using WhatsApp.
+     */
+    channelWhatsapp: z.boolean().optional(),
 });
 
 /**
@@ -37,10 +67,32 @@ export type SubscriptionsEndpoints =
 export type SubscriptionsRowModel = z.infer<typeof kSubscriptionRowModel>;
 
 /**
+ * Returns a comprehensive list of the subscription types that are available within the portal.
+ */
+async function getSubscriptionTypes() {
+    const teams = await db.selectFrom(tTeams)
+        .select({
+            id: tTeams.teamId,
+            name: tTeams.teamName,
+        })
+        .orderBy('name', 'asc')
+        .executeSelectMany();
+
+    return [
+        ...teams.map(team => ({
+            type: SubscriptionType.Application,
+            typeId: team.id,
+            label: `Application (${team.name})`,
+        })),
+        { type: SubscriptionType.Registration, typeId: null, label: 'New user registrations' },
+    ];
+}
+
+/**
  * The Subscription API is implemented as a regular, editable DataTable API. All operations are only
  * available to people with the SystemSubscriptionManagement privilege.
  */
-export const { DELETE, GET, POST, PUT } =
+export const { GET, PUT } =
 createDataTableApi(kSubscriptionRowModel, kSubscriptionContext, {
     accessCheck(request, action, props) {
         executeAccessCheck(props.authenticationContext, {
@@ -49,16 +101,76 @@ createDataTableApi(kSubscriptionRowModel, kSubscriptionContext, {
         });
     },
 
-    async create({ row }, props) {
-        return { success: false };
-    },
-
-    async delete({ id }) {
-        return { success: false };
-    },
-
     async list(context, props) {
-        return { success: false };
+        const subscriptionTypes = await getSubscriptionTypes();
+        const subscriptionsJoin = tSubscriptions.forUseInLeftJoin();
+
+        const dbInstance = db;
+        const volunteers = await dbInstance.selectFrom(tUsers)
+            .leftJoin(subscriptionsJoin)
+                .on(subscriptionsJoin.subscriptionUserId.equals(tUsers.userId))
+            .where(tUsers.privileges.greaterOrEquals(1n))
+                .and(tUsers.activated.equals(/* true= */ 1))
+            .select({
+                id: tUsers.userId,
+                name: tUsers.name,
+
+                subscriptions: dbInstance.aggregateAsArray({
+                    id: subscriptionsJoin.subscriptionId,
+                    type: subscriptionsJoin.subscriptionType,
+                    typeId: subscriptionsJoin.subscriptionTypeId,
+                    channelEmail: subscriptionsJoin.subscriptionChannelEmail.equals(1),
+                    channelNotification:
+                        subscriptionsJoin.subscriptionChannelNotification.equals(1),
+                    channelWhatsapp: subscriptionsJoin.subscriptionChannelWhatsapp.equals(1),
+                }),
+            })
+            .groupBy(tUsers.userId)
+            .orderBy('name', 'asc')
+            .executeSelectMany();
+
+        const subscriptions: SubscriptionsRowModel[] = [];
+        for (const volunteer of volunteers) {
+            const subscriptionCount = volunteer.subscriptions.length;
+            const subscriptionDescription =
+                `${subscriptionCount} subscription${subscriptionCount === 1 ? '' : 's'}`;
+
+            subscriptions.push({
+                id: `${volunteer.id}`,
+                name: volunteer.name,
+                description: subscriptionDescription,
+            });
+
+            for (const { type, typeId, label } of subscriptionTypes) {
+                let channelEmail: boolean = false;
+                let channelNotification: boolean = false;
+                let channelWhatsapp: boolean = false;
+
+                for (const subscription of volunteer.subscriptions) {
+                    if (subscription.type !== type || subscription.typeId !== typeId)
+                        continue;  // irrelevant subscription
+console.log('xxxx');
+                    channelEmail ||= subscription.channelEmail;
+                    channelNotification ||= subscription.channelNotification;
+                    channelWhatsapp ||= subscription.channelWhatsapp;
+                }
+
+                subscriptions.push({
+                    id: `${volunteer.id}/${type}/${typeId}`,
+                    name: label,
+                    type: type,
+                    channelEmail,
+                    channelNotification,
+                    channelWhatsapp,
+                });
+            }
+        }
+
+        return {
+            success: true,
+            rowCount: subscriptions.length,
+            rows: subscriptions,
+        };
     },
 
     async update({ row }, props) {
