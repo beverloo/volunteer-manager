9e656301c2c032a597b8d81173ea69a24585a5c0
Peter Beverloo <pbeverloo@gmail.com>
2024-04-07 20:45:03 +0100

    Add the ability to import sales data

--- app/admin/events/[slug]/settings/page.tsx
+++ app/admin/events/[slug]/settings/page.tsx
@@ -1,12 +1,18 @@
 // Copyright 2023 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
+import Link from 'next/link';
+
+import { default as MuiLink } from '@mui/material/Link';
+
 import type { NextPageParams } from '@lib/NextRouterParams';
 import { EventDeadlinesTable } from './EventDeadlinesTable';
+import { EventSales } from './EventSales';
 import { EventSettings } from './EventSettings';
 import { EventTeamsTable } from './EventTeamsTable';
-import { Privilege } from '@lib/auth/Privileges';
+import { Privilege, can } from '@lib/auth/Privileges';
 import { Section } from '@app/admin/components/Section';
+import { SectionIntroduction } from '@app/admin/components/SectionIntroduction';
 import { SettingsHeader } from './SettingsHeader';
 import { generateEventMetadataFn } from '../generateEventMetadataFn';
 import { verifyAccessAndFetchPageInfo } from '@app/admin/events/verifyAccessAndFetchPageInfo';
@@ -17,10 +23,13 @@ import { getLeadersForEvent } from '@app/admin/lib/getLeadersForEvent';
  * name, slug, target team sizes and so on. These have an effect on the entire Volunteer Manager.
  */
 export default async function EventSettingsPage(props: NextPageParams<'slug'>) {
-    const { event } = await verifyAccessAndFetchPageInfo(
+    const { event, user } = await verifyAccessAndFetchPageInfo(
         props.params, Privilege.EventAdministrator);
 
+    const isAdministrator = can(user, Privilege.Administrator);
     const leaders = await getLeadersForEvent(event.id);
+
+    const link = 'https://www.yourticketprovider.nl/account/events/manage/myevents.aspx';
 
     return (
         <>
@@ -34,6 +43,16 @@ export default async function EventSettingsPage(props: NextPageParams<'slug'>) {
             <Section title="Team settings">
                 <EventTeamsTable event={event} />
             </Section>
+            { !!isAdministrator &&
+                <Section title="Sales import">
+                    <SectionIntroduction>
+                        Aggregated information can be imported from our{' '}
+                        <MuiLink component={Link} href={link}>ticketing partner</MuiLink>:
+                        navigate to an event, then <em>Rapports</em>, then <em>Sales</em>, then
+                        download the daily statistics per ticket type.
+                    </SectionIntroduction>
+                    <EventSales event={event} />
+                </Section> }
         </>
     );
 }
---
+++ app/lib/database/scheme/EventsSalesTable.ts
@@ -0,0 +1,31 @@
+// @ts-nocheck
+/* eslint-disable quotes, max-len */
+/**
+ * DO NOT EDIT:
+ *
+ * This file has been auto-generated from database schema using ts-sql-codegen.
+ * Any changes will be overwritten.
+ */
+import { Table } from "ts-sql-query/Table";
+import type { DBConnection } from "../Connection";
+import {
+    TemporalTypeAdapter,
+} from "../TemporalTypeAdapter";
+import {
+    PlainDate,
+    ZonedDateTime,
+} from "../../Temporal";
+
+export class EventsSalesTable extends Table<DBConnection, 'EventsSalesTable'> {
+    eventId = this.column('event_id', 'int');
+    eventSaleDate = this.column<PlainDate>('event_sale_date', 'customLocalDate', 'date', TemporalTypeAdapter);
+    eventSaleType = this.column('event_sale_type', 'string');
+    eventSaleCount = this.column('event_sale_count', 'int');
+    eventSaleUpdated = this.column<ZonedDateTime>('event_sale_updated', 'customLocalDateTime', 'dateTime', TemporalTypeAdapter);
+
+    constructor() {
+        super('events_sales');
+    }
+}
+
+
