4b0af20533a3adb697e78b7001c92c4c6ac80735
Peter Beverloo <pbeverloo@gmail.com>
2024-04-11 21:30:10 +0100

    Update Next.js to v14.2.0

--- app/display/DisplayContainer.tsx
+++ app/display/DisplayContainer.tsx
@@ -1,6 +1,8 @@
 // Copyright 2024 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
+'use client';
+
 import Box from '@mui/material/Box';
 
 /**
