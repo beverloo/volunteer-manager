06fa74ab7fc9615b367c098a029be93810e62030
Peter Beverloo <pbeverloo@gmail.com>
2024-04-15 22:42:27 +0100

    Fix graphical artefacts when zooming the graph

--- app/admin/events/[slug]/EventSalesGraph.tsx
+++ app/admin/events/[slug]/EventSalesGraph.tsx
@@ -89,6 +89,17 @@ export interface EventSalesGraphProps {
 export function EventSalesGraph(props: EventSalesGraphProps) {
     const ref = useRef<SVGSVGElement | null>(null);
     useEffect(() => {
+        const root = d3.select(ref.current!);
+
+        // Remove all children that are currently part of the |root|, as we will be recreating all
+        // of them. This is likely required because of Next.js' SSR implementation.
+        root.selectChildren().remove();
+
+        // -----------------------------------------------------------------------------------------
+
+        const graphAreaWidth = kGraphWidth - kGraphMargins.left - kGraphMargins.right;
+        const graphAreaHeight = kGraphHeight - kGraphMargins.bottom;
+
         // Utility functions:
         const formattedBottomAxis = (scale: d3.ScaleLinear<number, number, number>) =>
             d3.axisBottom(scale)
@@ -101,27 +112,40 @@ export function EventSalesGraph(props: EventSalesGraphProps) {
         // -----------------------------------------------------------------------------------------
 
         // Select the root element and translate it:
-        const element = d3.select(ref.current!)
+        const element = root
             .append('g')
                 .attr('transform', `translate(${kGraphMargins.left}, ${kGraphMargins.top})`);
 
         // Add a horizontal axis:
         const scaleX = d3.scaleLinear()
             .domain([ props.xAxis.min, props.xAxis.max ])
-            .range([ 0, kGraphWidth - kGraphMargins.left - kGraphMargins.right ]);
+            .range([ 0, graphAreaWidth ]);
 
         const axisX = element.append('g')
-            .attr('transform', `translate(0, ${ kGraphHeight - kGraphMargins.bottom })`)
+            .attr('transform', `translate(0, ${ graphAreaHeight })`)
             .call(formattedBottomAxis(scaleX));
 
         // Add a vertical axis:
         const scaleY = d3.scaleLinear()
             .domain([ props.yAxis.max, props.yAxis.min ])
-            .range([ 0, kGraphHeight - kGraphMargins.bottom ])
+            .range([ 0, graphAreaHeight ])
             .nice();
 
         const axisY = element.append('g')
             .call(formattedLeftAxis(scaleY));
+
+        // Create a clip path:
+        const clip = element.append('defs')
+            .append('SVG:clipPath')
+            .attr('id', 'clip')
+            .append('SVG:rect')
+                .attr('width', graphAreaWidth)
+                .attr('height', graphAreaHeight)
+                .attr('x', 1)
+                .attr('y', 0);
+
+        const clippedContainer = element.append('g')
+            .attr('clip-path', 'url(#clip)');
 
         // -----------------------------------------------------------------------------------------
         // Data
@@ -129,12 +153,13 @@ export function EventSalesGraph(props: EventSalesGraphProps) {
 
         // Display each of the series:
         for (const serie of props.series) {
-            element.append('path')
+            clippedContainer.append('path')
                 .classed('data-entry', true)
                 .datum(serie.data)
                 .attr('fill', 'none')
                 .attr('stroke', serie.colour)
                 .attr('stroke-width', serie.width || 1.5)
+                .attr('style', 'vector-effect: non-scaling-stroke')
                 .attr('d', d3.line()
                     .x(entry => scaleX(entry[0]))
                     .y(entry => scaleY(entry[1]))
@@ -152,25 +177,23 @@ export function EventSalesGraph(props: EventSalesGraphProps) {
             axisX.call(formattedBottomAxis(updatedScaleX));
             axisY.call(formattedLeftAxis(updatedScaleY));
 
-            element.selectAll('.data-entry')
+            clippedContainer.selectAll('path')
                 .attr('transform', event.transform as any);
         }
 
         const zoom = d3.zoom()
-            .scaleExtent([ 0.5, 20 ])
+            .scaleExtent([ 1, 12 ])
             .extent([ [ 0, 0 ], [ kGraphWidth, kGraphHeight ] ])
             .on('zoom', adjustChartForZoom);
 
         element.append('rect')
-            .attr('width', kGraphWidth - kGraphMargins.left - kGraphMargins.right)
-            .attr('height', kGraphHeight - kGraphMargins.bottom)
+            .attr('width', graphAreaWidth)
+            .attr('height', graphAreaHeight)
             .style('fill', 'none')
             .style('pointer-events', 'all')
             .call(zoom as any);
 
         // TODO: Compute a confidence interval
-        // TODO: Fix overlapping elements when invalidating the graph
-        // TODO: Fix lack of clipping when zooming the graph
         // TODO: Enable a hover indicator
 
     }, [ props.series, props.xAxis, props.yAxis ]);
