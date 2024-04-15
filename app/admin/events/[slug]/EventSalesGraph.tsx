9c297b04b6292653136b59a45a22b28104437a98
Peter Beverloo <pbeverloo@gmail.com>
2024-04-15 22:18:10 +0100

    Enable the graph to be zoomed in and out

--- app/admin/events/[slug]/EventSalesGraph.tsx
+++ app/admin/events/[slug]/EventSalesGraph.tsx
@@ -89,45 +89,88 @@ export interface EventSalesGraphProps {
 export function EventSalesGraph(props: EventSalesGraphProps) {
     const ref = useRef<SVGSVGElement | null>(null);
     useEffect(() => {
+        // Utility functions:
+        const formattedBottomAxis = (scale: d3.ScaleLinear<number, number, number>) =>
+            d3.axisBottom(scale)
+                .ticks(kGraphTicks.horizontal)
+                .tickFormat(value => `${ !!props.xAxis.abs ? Math.abs(+value) : +value }`);
+
+        const formattedLeftAxis = (scale: d3.ScaleLinear<number, number, number>) =>
+            d3.axisLeft(scale).ticks(kGraphTicks.vertical);
+
+        // -----------------------------------------------------------------------------------------
+
+        // Select the root element and translate it:
         const element = d3.select(ref.current!)
             .append('g')
                 .attr('transform', `translate(${kGraphMargins.left}, ${kGraphMargins.top})`);
 
         // Add a horizontal axis:
-        const xScale = d3.scaleLinear()
+        const scaleX = d3.scaleLinear()
             .domain([ props.xAxis.min, props.xAxis.max ])
             .range([ 0, kGraphWidth - kGraphMargins.left - kGraphMargins.right ]);
 
-        element.append('g')
+        const axisX = element.append('g')
             .attr('transform', `translate(0, ${ kGraphHeight - kGraphMargins.bottom })`)
-            .call(d3.axisBottom(xScale)
-                .ticks(kGraphTicks.horizontal)
-                .tickFormat(value => `${ !!props.xAxis.abs ? Math.abs(+value) : +value }`));
+            .call(formattedBottomAxis(scaleX));
 
         // Add a vertical axis:
-        const yScale = d3.scaleLinear()
+        const scaleY = d3.scaleLinear()
             .domain([ props.yAxis.max, props.yAxis.min ])
             .range([ 0, kGraphHeight - kGraphMargins.bottom ])
             .nice();
 
-        element.append('g')
-            .call(d3.axisLeft(yScale).ticks(kGraphTicks.vertical));
+        const axisY = element.append('g')
+            .call(formattedLeftAxis(scaleY));
+
+        // -----------------------------------------------------------------------------------------
+        // Data
+        // -----------------------------------------------------------------------------------------
 
         // Display each of the series:
         for (const serie of props.series) {
             element.append('path')
+                .classed('data-entry', true)
                 .datum(serie.data)
                 .attr('fill', 'none')
                 .attr('stroke', serie.colour)
                 .attr('stroke-width', serie.width || 1.5)
                 .attr('d', d3.line()
-                    .x(entry => xScale(entry[0]))
-                    .y(entry => yScale(entry[1]))
+                    .x(entry => scaleX(entry[0]))
+                    .y(entry => scaleY(entry[1]))
                     .curve(d3.curveCardinal.tension(0.25)));
         }
 
+        // -----------------------------------------------------------------------------------------
+        // Interactivity
+        // -----------------------------------------------------------------------------------------
+
+        function adjustChartForZoom(event: d3.D3ZoomEvent<SVGRectElement, unknown>) {
+            const updatedScaleX = event.transform.rescaleX(scaleX);
+            const updatedScaleY = event.transform.rescaleY(scaleY);
+
+            axisX.call(formattedBottomAxis(updatedScaleX));
+            axisY.call(formattedLeftAxis(updatedScaleY));
+
+            element.selectAll('.data-entry')
+                .attr('transform', event.transform as any);
+        }
+
+        const zoom = d3.zoom()
+            .scaleExtent([ 0.5, 20 ])
+            .extent([ [ 0, 0 ], [ kGraphWidth, kGraphHeight ] ])
+            .on('zoom', adjustChartForZoom);
+
+        element.append('rect')
+            .attr('width', kGraphWidth - kGraphMargins.left - kGraphMargins.right)
+            .attr('height', kGraphHeight - kGraphMargins.bottom)
+            .style('fill', 'none')
+            .style('pointer-events', 'all')
+            .call(zoom as any);
+
         // TODO: Compute a confidence interval
-        // TODO: Enable zooming in on the data
+        // TODO: Fix overlapping elements when invalidating the graph
+        // TODO: Fix lack of clipping when zooming the graph
         // TODO: Enable a hover indicator
 
     }, [ props.series, props.xAxis, props.yAxis ]);
