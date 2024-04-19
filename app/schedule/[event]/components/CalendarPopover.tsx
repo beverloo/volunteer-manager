0adfa00c9226a9fb636fab4f12d159036de633ed
Peter Beverloo <pbeverloo@gmail.com>
2024-04-19 20:07:48 +0100

    Restore mobile padding for the mobile calendar

--- app/schedule/[event]/components/CalendarPopover.tsx
+++ app/schedule/[event]/components/CalendarPopover.tsx
@@ -106,7 +106,7 @@ export default function CalendarPopover(props: CalendarPopoverProps) {
     return (
         <Dialog open={!!open} onClose={onClose} fullScreen={isMobile} fullWidth maxWidth="md">
             <Stack direction="row" justifyContent="space-between" alignItems="center">
-                <DialogTitle sx={{ pb: 0 }}>
+                <DialogTitle sx={{ pb: { xs: 2, md: 0 } }}>
                     {title}
                 </DialogTitle>
                 <Box sx={{ pr: 2 }}>
