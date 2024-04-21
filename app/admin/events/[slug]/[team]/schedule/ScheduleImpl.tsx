7ecaa8f16ec8b2ab42162edeb0430386feb0e71c
Peter Beverloo <pbeverloo@gmail.com>
2024-04-21 11:40:49 +0100

    Plumbing to support interaction on the schedule

--- app/admin/events/[slug]/[team]/schedule/ScheduleImpl.tsx
+++ app/admin/events/[slug]/[team]/schedule/ScheduleImpl.tsx
@@ -3,16 +3,20 @@
 
 'use client';
 
-import { useContext, useMemo } from 'react';
+import { useCallback, useContext, useMemo, useState } from 'react';
 
+import type { PopoverPosition } from '@mui/material/Popover';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Paper from '@mui/material/Paper';
 import Skeleton from '@mui/material/Skeleton';
 
-import type { ScheduleEvent, ScheduleMarker, ScheduleResource }
+import type { ScheduleEvent, ScheduleEventMutation, ScheduleMarker, ScheduleResource }
     from '@app/admin/components/Schedule';
 
 import { ScheduleContext } from './ScheduleContext';
 import { Schedule } from '@app/admin/components/Schedule';
+import { SettingDialog } from '@app/admin/components/SettingDialog';
 
 /**
  * Background color to issue to markers on the schedule, depending on what they represent.
@@ -77,8 +81,76 @@ export function ScheduleImpl(props: ScheduleImplProps) {
 
     // ---------------------------------------------------------------------------------------------
 
-    // TODO: Interaction (double click)
-    // TODO: Interaction (right click)
+    type DialogResolver = undefined | ((mutation?: ScheduleEventMutation) => void);
+
+    const [ dialogEvent, setDialogEvent ] = useState<ScheduleEvent | undefined>();
+    const [ dialogResolver, setDialogResolver ] = useState<DialogResolver>();
+
+    // TODO: Populate a list of shifts that can be selected
+    // TODO: Hook up updates in the `handleDialogUpdate` callback
+
+    const handleDialogClose = useCallback(() => {
+        setDialogEvent(undefined);
+        if (!!dialogResolver)
+            dialogResolver(/* do nothing */);
+
+    }, [ dialogResolver ]);
+
+    const handleDialogDelete = useCallback(async () => {
+        console.log(dialogEvent, dialogResolver);
+        if (!dialogEvent || !dialogResolver)
+            return { error: <>I forgot which shift was selected, sorry!</> };
+
+        dialogResolver({ delete: true });
+        return { close: true } as const;
+
+    }, [ dialogEvent, dialogResolver ]);
+
+    const handleDialogUpdate = useCallback(async (event: ScheduleEvent) => {
+        return { error: 'Not yet implemented' };
+
+    }, [ dialogEvent ]);
+
+    const handleDoubleClick = useCallback(async (event: ScheduleEvent) => {
+        return new Promise<ScheduleEventMutation | undefined>(resolve => {
+            setDialogEvent(event);
+            setDialogResolver(() => resolve);
+        });
+    }, [ /* no dependencies */ ]);
+
+    // ---------------------------------------------------------------------------------------------
+
+    type ContextMenuResolver = undefined | ((mutation?: ScheduleEventMutation) => void);
+
+    const [ contextMenuEvent, setContextMenuEvent ] = useState<ScheduleEvent | undefined>();
+    const [ contextMenuPosition, setContextMenuPosition ] = useState<PopoverPosition | undefined>();
+    const [ contextMenuResolver, setContextMenuResolver ] = useState<ContextMenuResolver>();
+
+    // TODO: Populate a list of the most recent selected shifts
+    // TODO: Hook up selecting a particular shift to udpate the `contextMenuEvent` to
+
+    const handleRightClickMenuClose = useCallback(() => {
+        setContextMenuEvent(undefined);
+        if (!!contextMenuResolver)
+            contextMenuResolver(/* do nothing */);
+
+    }, [ contextMenuResolver ]);
+
+    const handleRightClick = useCallback(
+        async (event: ScheduleEvent, domEvent: React.MouseEvent<HTMLElement>) => {
+            return new Promise<ScheduleEventMutation | undefined>(resolve => {
+                setContextMenuEvent(event);
+                setContextMenuPosition({
+                    top: domEvent.clientY,
+                    left: domEvent.clientX,
+                });
+
+                setContextMenuResolver(() => resolve);
+            });
+
+        }, [ /* no dependencies */ ]);
+
+    // ---------------------------------------------------------------------------------------------
 
     if (!context.schedule || !resources.length) {
         return (
@@ -100,8 +172,23 @@ export function ScheduleImpl(props: ScheduleImplProps) {
         <Paper>
             <Schedule min={context.schedule.min} max={context.schedule.max} readOnly={readOnly}
                       events={events} eventDefaults={eventDefaults} eventOverlap={false}
-                      onChange={context.processMutation} markers={markers} resources={resources}
+                      onChange={context.processMutation} onRightClick={handleRightClick}
+                      onDoubleClick={handleDoubleClick} markers={markers} resources={resources}
                       displayTimezone={context.schedule.timezone} subject="shift" />
+            <SettingDialog title="Select a shift" delete open={!!dialogEvent}
+                           onClose={handleDialogClose} onDelete={handleDialogDelete}
+                           onSubmit={handleDialogUpdate} defaultValues={ dialogEvent ?? {} }>
+                <em>Shift selection dialog will show up here</em>
+            </SettingDialog>
+            <Menu open={!!contextMenuEvent}
+                  onClose={handleRightClickMenuClose}
+                  anchorReference="anchorPosition"
+                  anchorPosition={contextMenuPosition}>
+                <MenuItem>Recent shift #1</MenuItem>
+                <MenuItem>Recent shift #2</MenuItem>
+                <MenuItem>Recent shift #3</MenuItem>
+                <MenuItem>Recent shift #4</MenuItem>
+            </Menu>
         </Paper>
     );
 }
