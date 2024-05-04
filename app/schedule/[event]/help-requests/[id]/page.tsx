5c3ff26910104e574c74b76880a3bf60c269ec12
Peter Beverloo <pbeverloo@gmail.com>
2024-05-04 15:51:25 +0100

    Placeholder page for an individual help request

--- app/schedule/[event]/help-requests/[id]/page.tsx
+++ app/schedule/[event]/help-requests/[id]/page.tsx
@@ -1,11 +1,12 @@
 // Copyright 2024 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
 
 import type { NextPageParams } from '@lib/NextRouterParams';
 import { Privilege } from '@lib/auth/Privileges';
-import { Section } from '../../components/Section';
+import { SetTitle } from '../../components/SetTitle';
 import { requireAuthenticationContext } from '@lib/auth/AuthenticationContext';
 
 /**
@@ -21,10 +22,21 @@ export default async function ScheduleHelpRequestPage(props: NextPageParams<'eve
     });
 
     return (
-        <Section>
-            <Typography variant="body1">
-                This page is not available yet (/help-requests/:id)
-            </Typography>
-        </Section>
+        <>
+            <SetTitle title="Help request" />
+            <Card>
+                <Box sx={{
+                    backgroundImage: 'url(/images/help-request.jpg)',
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    width: '100%',
+                    aspectRatio: 4 }} />
+            </Card>
+            { /* TODO: Show information about the request */ }
+            { /* TODO: Show information about who acknowledged the request */ }
+            { /* TODO: Ability to acknowledge the request if that hasn't happened yet */ }
+            { /* TODO: Show information about who closed the request, and why */ }
+            { /* TODO: Ability to close the request if that hasn't happened yet */ }
+        </>
     );
 }
