c8fa846589827776e4f7ed1037a973f0c70c3670
Peter Beverloo <pbeverloo@gmail.com>
2024-05-25 12:52:31 +0100

    feat: Enable updating the schedule's palette mode through a context

--- app/schedule/[event]/ScheduleTheme.tsx
+++ app/schedule/[event]/ScheduleTheme.tsx
@@ -3,6 +3,9 @@
 
 'use client';
 
+import { createContext, useCallback, useMemo, useState } from 'react';
+
+import type { PaletteMode } from '@mui/material';
 import type { SxProps } from '@mui/system';
 import type { Theme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
@@ -10,6 +13,40 @@ import { createTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 import Box from '@mui/material/Box';
+
+/**
+ * Palette mode that the schedule app can be in. Extends the regular MUI palette.
+ */
+type SchedulePaletteMode = PaletteMode | 'auto';
+
+/**
+ * Data stored within the theme context.
+ */
+export interface ScheduleThemeContextInfo {
+    /**
+     * Whether the schedule app should be in dark mode, light mode, or be inferred.
+     */
+    mode: SchedulePaletteMode;
+
+    /**
+     * Updates the palette mode for the schedule. Will persist between page loads for the local
+     * client, but not across clients for the same user.
+     */
+    updateMode?: (mode: SchedulePaletteMode) => void;
+}
+
+/**
+ * The <ScheduleThemeContext> carries information about the theme that can be accessed and
+ * manipulated throughout the volunteer portal.
+ */
+export const ScheduleThemeContext = createContext<ScheduleThemeContextInfo>({
+    mode: 'auto',
+});
+
+/**
+ * Cache key in the local storage in which we'll store the configured palette mode.
+ */
+const kCacheKey = 'acPalette';
 
 /**
  * Styling rules for the <ScheduleTheme> component & hierarchy.
@@ -26,6 +63,44 @@ const kStyles: { [key: string]: SxProps<Theme> } = {
         colorScheme: 'light only',
     },
 };
+
+/**
+ * Reads the palette mode from local cache, or `auto` as the default value when none has been set.
+ */
+function readPaletteModeFromCache(): SchedulePaletteMode {
+    if (typeof globalThis.localStorage === 'undefined')
+        return 'auto';   // bail out for server-side rendering
+
+    try {
+        const paletteMode = localStorage.getItem(kCacheKey);
+        switch (paletteMode) {
+            case 'light':
+            case 'dark':
+                return paletteMode;
+        }
+    } finally { /* nothing to do */ }
+
+    return 'auto';
+}
+
+/**
+ * Stores the given `mode` as the locally cached palette mode. It's not clear whether the try/catch
+ * is necessary here, it's added due to historic trauma with Private Mode in iOS Safari.
+ */
+function storePaletteModeToCache(mode: SchedulePaletteMode) {
+    try {
+        switch (mode) {
+            case 'light':
+            case 'dark':
+                localStorage.setItem(kCacheKey, mode);
+                break;
+
+            default:
+                localStorage.removeItem(kCacheKey);
+                break;
+        }
+    } finally { /* nothing to do */ }
+}
 
 /**
  * Creates the theme for the schedule environment.
@@ -47,11 +122,36 @@ function createScheduleTheme(darkMode?: boolean) {
  */
 export function ScheduleTheme(props: React.PropsWithChildren) {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const [ mode, setMode ] = useState<SchedulePaletteMode>(() => readPaletteModeFromCache());
+
+    const updateMode = useCallback((mode: SchedulePaletteMode) => {
+        storePaletteModeToCache(mode);
+        setMode(mode);
+    }, [ /* no deps */ ]);
+
+    const context = useMemo((): ScheduleThemeContextInfo => {
+        return { mode, updateMode };
+    }, [ mode, updateMode ]);
+
+    const theme = useMemo(() => {
+        switch (mode) {
+            case 'light':
+            case 'dark':
+                return createScheduleTheme(mode === 'dark');
+
+            default:
+                return createScheduleTheme(!!prefersDarkMode);
+        }
+    }, [ mode, prefersDarkMode ]);
+
     return (
-        <ThemeProvider theme={createScheduleTheme(!!prefersDarkMode)}>
-            <Box sx={kStyles.container}>
-                {props.children}
-            </Box>
-        </ThemeProvider>
+        <ScheduleThemeContext.Provider value={context}>
+            <ThemeProvider theme={theme}>
+                <Box sx={kStyles.container}>
+                    {props.children}
+                </Box>
+            </ThemeProvider>
+        </ScheduleThemeContext.Provider>
     );
 }
