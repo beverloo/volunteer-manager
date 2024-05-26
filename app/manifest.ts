3e06e8044ab06c9741df5dd6d67250022571bfc5
Peter Beverloo <pbeverloo@gmail.com>
2024-05-26 15:11:53 +0100

    feat: Add icons to the manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -32,7 +32,23 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         display: 'standalone',
         background_color: '#ffffff',
         theme_color: environment?.themeColours.light || '#303f9f',
-        // TODO: icons
+        icons: [
+            {
+                src: `/images/${environment?.environmentName}/logo-512.png`,
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: `/images/${environment?.environmentName}/logo-256.png`,
+                sizes: '256x256',
+                type: 'image/png',
+            },
+            {
+                src: `/images/${environment?.environmentName}/logo-192.png`,
+                sizes: '192x192',
+                type: 'image/png',
+            },
+        ],
         // TODO: screenshots
         // TODO: shortcuts
     }
