baca9c326a4463e24bb75a5750c4118142683a6f
Peter Beverloo <pbeverloo@gmail.com>
2024-06-18 16:44:20 +0100

    Implement basic type coercion and validation in `serverAction()`

--- app/lib/serverAction.test.ts
+++ app/lib/serverAction.test.ts
@@ -15,25 +15,272 @@ describe('serverAction', () => {
     }
 
     it('should reject invalid form data types', async () => {
-        // TODO: Implement this test
+        let invocations = 0;
+
+        const scheme = z.object({ /* empty */ });
+        const action = serverAction(scheme, async (data, props) => { ++invocations; });
+
+        {
+            const result = await action(/* formData= */ undefined as any);
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            !result.success && expect(result.error).toBe('Invalid form data received from Next.js');
+        }
+        {
+            const result = await action(/* formData= */ null as any);
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            !result.success && expect(result.error).toBe('Invalid form data received from Next.js');
+        }
+        {
+            const result = await action(/* formData= */ {} as any);
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            !result.success && expect(result.error).toBe('Invalid form data received from Next.js');
+        }
+        {
+            const result = await action(/* formData= */ new FormData);
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(1);
+        }
     });
 
     it('should reject form data with missing fields', async () => {
-        // TODO: Implement this test
+        let invocations = 0;
+
+        const scheme = z.object({
+            a: z.number(),
+            b: z.number(),
+        });
+
+        const action = serverAction(scheme, async (data, props) => { ++invocations; });
+
+        {
+            const result = await action(toFormData({ a: 42 }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ b: 101 }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: 42, b: 101 }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(1);
+        }
     });
 
     it('should reject form data with fields having invalid types', async () => {
+        let invocations = 0;
+
         const scheme = z.object({
-            foo: z.number(),
+            a: z.number(),
+            b: z.boolean(),
         });
 
-        const action = serverAction(scheme, async (data, props) => { /* void */ });
-        const result = await action(toFormData({
-            foo: 'bar',
-        }));
+        const action = serverAction(scheme, async (data, props) => { ++invocations; });
 
-        expect(result).toBeObject();
-        expect(result.success).toBeFalse();
+        {
+            const result = await action(toFormData({ a: 42, b: 121 }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: '42', b: 121 }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: 42, b: true }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(1);
+        }
+    });
+
+    it('should be able to coerce booleans from FormData input', async () => {
+        let a: boolean | undefined;
+        let invocations = 0;
+
+        const scheme = z.object({
+            a: z.boolean(),
+        });
+
+        const action = serverAction(scheme, async (data, props) => { a = data.a; ++invocations; });
+
+        {
+            const result = await action(toFormData({ a: 42 }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ /* missing property */ }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: { /* object */ } }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: undefined }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: null }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: 'on' }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(1);
+            expect(a).toBeTrue();
+        }
+        {
+            const result = await action(toFormData({ a: 'true' }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(2);
+            expect(a).toBeTrue();
+        }
+        {
+            const result = await action(toFormData({ a: /* "true"= */ true }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(3);
+            expect(a).toBeTrue();
+        }
+        {
+            const result = await action(toFormData({ a: 'off' }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(4);
+            expect(a).toBeFalse();
+        }
+        {
+            const result = await action(toFormData({ a: 'false' }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(5);
+            expect(a).toBeFalse();
+        }
+        {
+            const result = await action(toFormData({ a: false }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(6);
+            expect(a).toBeFalse();
+        }
+    });
+
+    it('should be able to coerce numbers from FormData input', async () => {
+        let a: number | undefined;
+        let invocations = 0;
+
+        const scheme = z.object({
+            a: z.number(),
+        });
+
+        const action = serverAction(scheme, async (data, props) => { a = data.a; ++invocations; });
+
+        {
+            const result = await action(toFormData({ a: 'value' }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: { /* object */ } }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: Number.NaN }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: 'NaN' }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: undefined }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: null }));
+            expect(result.success).toBeFalse();
+            expect(invocations).toBe(0);
+
+            // TODO: Validate the error message
+        }
+        {
+            const result = await action(toFormData({ a: 42 }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(1);
+            expect(a).toBe(42);
+        }
+        {
+            const result = await action(toFormData({ a: -42.25 }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(2);
+            expect(a).toBe(-42.25);
+        }
+        {
+            const result = await action(toFormData({ a: Number.MAX_SAFE_INTEGER }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(3);
+            expect(a).toBe(Number.MAX_SAFE_INTEGER);
+        }
+        {
+            const result = await action(toFormData({ a: Number.MIN_SAFE_INTEGER }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(4);
+            expect(a).toBe(Number.MIN_SAFE_INTEGER);
+        }
+        {
+            const result = await action(toFormData({ a: Number.POSITIVE_INFINITY }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(5);
+            expect(a).toBe(Number.POSITIVE_INFINITY);
+        }
+        {
+            const result = await action(toFormData({ a: Number.NEGATIVE_INFINITY }));
+            expect(result.success).toBeTrue();
+            expect(invocations).toBe(6);
+            expect(a).toBe(Number.NEGATIVE_INFINITY);
+        }
     });
 
     it('should be able to transform the input/output using Zod', async () => {
--- app/lib/serverAction.ts
+++ app/lib/serverAction.ts
@@ -1,7 +1,8 @@
 // Copyright 2024 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import type { AnyZodObject, z } from 'zod';
+import type { ZodObject, ZodRawShape, ZodTypeDef, z } from 'zod';
+import { ZodError, ZodFirstPartyTypeKind } from 'zod';
 
 /**
  * Type definition for the props that will be made available to a server action implementation.
@@ -45,8 +46,85 @@ type ServerAction = (formData: FormData) => Promise<ServerActionResult>;
  * Type definition that represents a React server action. When the action does not return any value
  * success will be assumed, whereas exceptions will be represented as a failure.
  */
-type ServerActionImplementation<T extends AnyZodObject> =
-    (data: z.output<T>, props: ServerActionProps) => Promise<ServerActionResult | void>;
+type ServerActionImplementation<T extends ZodObject<ZodRawShape, any, any>> =
+    (data: z.output<T>, props: ServerActionProps) => Promise<ServerActionResult | undefined | void>;
+
+/**
+ * Types that the `coerceZodType` function can coerce string values to.
+ */
+type ServerActionCoercedTypes = boolean | number | string | undefined;
+
+/**
+ * Coerces the given `value` to either a boolean, a string when it has an invalid value, or the
+ * `undefined` value when it is something unparseable such as a file.
+ */
+function coerceZodBoolean(value: FormDataEntryValue): boolean | string | undefined {
+    switch (value) {
+        case 'on':  // checkboxes
+        case 'true':
+            return true;
+
+        case 'off':  // for consistency
+        case 'false':
+            return false;
+
+        default:
+            return typeof value === 'string' ? value : undefined;
+    }
+}
+
+/**
+ * Coerces the given `value` to the type represented by the given `def`, which is a Zod type
+ * definition. The `typeName` field is not included in the base definition, but always exists.
+ */
+function coerceZodType(def: any, values: FormDataEntryValue[]): ServerActionCoercedTypes {
+    switch (def.typeName) {
+        case ZodFirstPartyTypeKind.ZodBoolean:
+            return coerceZodBoolean(values[0]);
+        case ZodFirstPartyTypeKind.ZodNumber:
+            return Number(values[0]);
+    }
+
+    // TODO: Coerce arrays
+    // TODO: Coerce bigints
+    // TODO: Coerce files
+    // TODO: Coerce ...
+
+    return typeof values[0] === 'string' ? values[0] : '_skip_';
+}
+
+/**
+ * Coerces the given `formData` into an object that roughly corresponds to the `scheme` without
+ * invoking processing and transformation functions. This allows certain types such as arrays,
+ * booleans and numbers to work as expected.
+ */
+function coerceFormData<T extends ZodObject<ZodRawShape, any, any>>(scheme: T, formData: FormData) {
+    const unvalidatedData: Record<string, ServerActionCoercedTypes> = { /* empty */ };
+    for (const key of formData.keys()) {
+        const values = formData.getAll(key);
+        if (Object.hasOwn(scheme.shape, key)) {
+            if (values.length > 0)
+                unvalidatedData[key] = coerceZodType(scheme.shape[key]._def, values);
+            else
+                unvalidatedData[key] = undefined;
+
+        } else if (values.length >= 1 && typeof values[0] === 'string') {
+            // Only string values are supported as arbitrary key/value pass-through.
+            unvalidatedData[key] = values[0];
+        }
+    }
+
+    return unvalidatedData;
+}
+
+/**
+ * Function that takes a Zod-created `error` and formats it into a single string, that's slightly
+ * more user readable than the `error`'s own message.
+ */
+export function formatZodError(error: ZodError): string {
+    // TODO: Implement this method.
+    return error.message;
+}
 
 /**
  * Function that wraps an asynchronous `action` to provide consistent input and output, as well as
@@ -57,16 +135,31 @@ type ServerActionImplementation<T extends AnyZodObject> =
  * takes care of version skew and common attack types, which we build upon by automatically
  * authenticating the user.
  */
-export function serverAction<T extends AnyZodObject>(
+export function serverAction<T extends ZodObject<ZodRawShape, any, any>>(
     scheme: T, action: ServerActionImplementation<T>): ServerAction
 {
     return async (formData: FormData) => {
-        // TODO: Validate the incoming `formData` and transform it in a typed structure.
-        // TODO: Invoke the `action` when validation has succeeded.
+        if (!(formData instanceof FormData))
+            return { success: false, error: 'Invalid form data received from Next.js' };
 
-        return {
-            success: false,
-            error: 'Not yet implemented',
+        try {
+            const data = scheme.parse(coerceFormData(scheme, formData));
+            const props: ServerActionProps = {
+                // TODO: Populate the props
+            };
+
+            const result = await action(data, props);
+            if (typeof result === 'object')
+                return result;
+
+            return { success: true };
+
+        } catch (error: any) {
+            if (error instanceof ZodError)
+                return { success: false, error: formatZodError(error) };
+
+            // TODO: Implement support for other exception types
+            return { success: false, error: error.message };
         }
     };
 }
