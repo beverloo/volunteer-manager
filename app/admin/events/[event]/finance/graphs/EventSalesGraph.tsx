b46a896bca14da4a3df7496166515620cf5127f3
Peter Beverloo <pbeverloo@gmail.com>
2025-02-23 10:19:43 +0000

    feat: Factor out common chart behaviour to a base component

--- app/admin/events/[event]/finance/graphs/EventSalesGraph.tsx
+++ app/admin/events/[event]/finance/graphs/EventSalesGraph.tsx
@@ -1,23 +1,12 @@
 // Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import { useId } from 'react';
-
-import { ResponsiveChartContainer, type ResponsiveChartContainerProps } from '@mui/x-charts/ResponsiveChartContainer';
-import { ChartsClipPath } from '@mui/x-charts/ChartsClipPath';
-import { ChartsAxisHighlight } from '@mui/x-charts/ChartsAxisHighlight';
-import { ChartsReferenceLine } from '@mui/x-charts/ChartsReferenceLine';
-import { ChartsTooltip } from '@mui/x-charts/ChartsTooltip/ChartsTooltip';
-import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
-import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
-import { LinePlot } from '@mui/x-charts/LineChart';
-import Typography from '@mui/material/Typography';
+import type { ResponsiveChartContainerProps } from '@mui/x-charts/ResponsiveChartContainer';
 
 import type { EventSalesCategory } from '@lib/database/Types';
+import { SalesGraph } from './SalesGraph';
 import { Temporal, isBefore, isAfter } from '@lib/Temporal';
 import db, { tActivities, tEventsSales } from '@lib/database';
-
-import { kMaximumColor, kTodayColor } from './GraphCommon';
 
 /**
  * Props accepted by the <EventSalesGraph> component.
@@ -60,16 +49,12 @@ export interface EventSalesGraphProps {
  * of one of our festivals. Sales of this kind may have a limit set as well.
  */
 export async function EventSalesGraph(props: EventSalesGraphProps) {
-    const today = Temporal.Now.plainDateISO();
     const start = Temporal.PlainDate.from(props.range[0]);
     const end = Temporal.PlainDate.from(props.range[1]);
 
-    const clipPathId = useId();
     const dbInstance = db;
 
     // TODO: Enable linking the user through to the associated activity
-    // TODO: Enable hover dialogs to indicate what's being looked at
-    // TODO: Consider enabling a grid on the graph, for additional clarity
 
     // ---------------------------------------------------------------------------------------------
     // Determine the title of the graph. When an Activity ID is given this will be used, with the
@@ -135,7 +120,8 @@ export async function EventSalesGraph(props: EventSalesGraphProps) {
                 if (productSales !== undefined && productSales > 0)
                     totalProductSales += productSales;
 
-                data.push(totalProductSales);
+                data.push(totalProductSales > 0 ? totalProductSales
+                                                : null);
             }
 
             series.push({
@@ -148,33 +134,14 @@ export async function EventSalesGraph(props: EventSalesGraphProps) {
 
     // ---------------------------------------------------------------------------------------------
 
+    const currentPlainDate = Temporal.Now.plainDateISO();
+    const today =
+        (!isBefore(currentPlainDate, start) && !isAfter(currentPlainDate, end))
+            ? currentPlainDate.toString()
+            : undefined;
+
     return (
-        <>
-            <Typography variant="h5">
-                {title}
-            </Typography>
-            <ResponsiveChartContainer series={series} height={300} margin={{ top: 24 }}
-                                      xAxis={[ { scaleType: 'point', data: xLabels } ]}>
-                <g clipPath={clipPathId}>
-                    <LinePlot />
-                    { (!isBefore(today, start) && !isAfter(today, end)) &&
-                        <ChartsReferenceLine x={today.toString()}
-                                            lineStyle={{
-                                                stroke: kTodayColor
-                                            }} /> }
-                    { !!props.limit &&
-                        <ChartsReferenceLine y={props.limit}
-                                            lineStyle={{
-                                                strokeDasharray: 4,
-                                                stroke: kMaximumColor
-                                            }} /> }
-                </g>
-                <ChartsAxisHighlight x="line" />
-                <ChartsTooltip />
-                <ChartsXAxis />
-                <ChartsYAxis />
-                <ChartsClipPath id={clipPathId} />
-            </ResponsiveChartContainer>
-        </>
+        <SalesGraph limit={props.limit} series={series} title={title} today={today}
+                    xLabels={xLabels} />
     );
 }
---
+++ app/admin/events/[event]/finance/graphs/SalesGraph.tsx
@@ -0,0 +1,96 @@
+// Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+import { useId } from 'react';
+
+import { ResponsiveChartContainer, type ResponsiveChartContainerProps } from '@mui/x-charts/ResponsiveChartContainer';
+import { ChartsAxisHighlight } from '@mui/x-charts/ChartsAxisHighlight';
+import { ChartsClipPath } from '@mui/x-charts/ChartsClipPath';
+import { ChartsGrid } from '@mui/x-charts/ChartsGrid';
+import { ChartsReferenceLine } from '@mui/x-charts/ChartsReferenceLine';
+import { ChartsTooltip } from '@mui/x-charts/ChartsTooltip/ChartsTooltip';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
+import { LinePlot } from '@mui/x-charts/LineChart';
+import Typography from '@mui/material/Typography';
+
+/**
+ * Colour in which the line visualising maximum ticket sales should be displayed.
+ */
+const kMaximumColor = '#C62828';
+
+/**
+ * Colour in which the line visualising today's date should be displayed.
+ */
+const kTodayColor = '#1976D2';
+
+/**
+ * Props accepted by the <SalesGraph> component.
+ */
+export interface SalesGraphProps {
+    /**
+     * Optional limit indicating the maximum number of tickets that can be sold.
+     */
+    limit?: number;
+
+    /**
+     * Series that should be displayed on the graph.
+     */
+    series: ResponsiveChartContainerProps['series'];
+
+    /**
+     * Title that should be displayed on the graph, if any.
+     */
+    title?: string;
+
+    /**
+     * Date on which the "today" vertical line should be displayed.
+     */
+    today?: string;
+
+    /**
+     * Labels that should be displayed on the x-axis of the graph.
+     */
+    xLabels: string[];
+}
+
+/**
+ * The <SalesGraph> component renders a graph with settings consistent to how we would like graphs
+ * to work in the Volunteer Manager. Input are the series, axis contents, and optional configuration
+ * to specialise the default display.
+ */
+export async function SalesGraph(props: SalesGraphProps) {
+    const clipPathId = useId();
+
+    return (
+        <>
+            { !!props.title &&
+                <Typography variant="h5">
+                    {props.title}
+                </Typography> }
+            <ResponsiveChartContainer series={props.series} height={300} margin={{ top: 24 }}
+                                      xAxis={[ { scaleType: 'point', data: props.xLabels } ]}>
+                <g clipPath={clipPathId}>
+                    <LinePlot />
+                    { !!props.today &&
+                        <ChartsReferenceLine x={props.today}
+                                            lineStyle={{
+                                                stroke: kTodayColor
+                                            }} /> }
+                    { !!props.limit &&
+                        <ChartsReferenceLine y={props.limit}
+                                            lineStyle={{
+                                                strokeDasharray: 4,
+                                                stroke: kMaximumColor
+                                            }} /> }
+                </g>
+                <ChartsAxisHighlight x="line" />
+                <ChartsGrid horizontal={true} />
+                <ChartsTooltip />
+                <ChartsXAxis />
+                <ChartsYAxis />
+                <ChartsClipPath id={clipPathId} />
+            </ResponsiveChartContainer>
+        </>
+    );
+}
