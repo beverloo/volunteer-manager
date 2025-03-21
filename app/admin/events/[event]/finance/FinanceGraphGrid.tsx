5be8b2a583672c5cb00fe0afa92f28110031fbcb
Peter Beverloo <pbeverloo@gmail.com>
2025-03-21 18:24:59 +0000

    faff: Factor out the finance graph selection to a separate component

---
+++ app/admin/events/[event]/finance/FinanceGraphGrid.tsx
@@ -0,0 +1,130 @@
+// Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
+import { Suspense } from 'react';
+
+import Alert from '@mui/material/Alert';
+import Grid from '@mui/material/Grid2';
+import Paper from '@mui/material/Paper';
+
+import { type EventSalesGraphProps, EventSalesGraph } from './graphs/EventSalesGraph';
+import { type TicketSalesGraphProps, TicketSalesGraph } from './graphs/TicketSalesGraph';
+import { LoadingGraph } from './graphs/LoadingGraph';
+import { Section } from '@app/admin/components/Section';
+import { selectRangeForEvent } from './graphs/SalesGraphUtils';
+import db, { tEventsSalesConfiguration } from '@lib/database';
+
+import { kEventSalesCategory } from '@lib/database/Types';
+
+/**
+ * Props accepted by the <FinanceGraphGrid> component.
+ */
+interface FinanceGraphGridProps {
+    /**
+     * Unique ID of the event for which graphs should be shown.
+     */
+    eventId: number;
+}
+
+/**
+ * The <FinanceGraphGrid> component displays a grid with a series of financial graphs related to a
+ * particular event. No access checks are performed by this component.
+ */
+export async function FinanceGraphGrid(props: FinanceGraphGridProps) {
+    const { eventId } = props;
+
+    // ---------------------------------------------------------------------------------------------
+    // Determine the date ranges that should be displayed on the graphs. All graphs will show data
+    // along the same X axes to make sure that they're visually comparable.
+
+    const range = await selectRangeForEvent(eventId);
+
+    // ---------------------------------------------------------------------------------------------
+    // Determine that graphs that have to be displayed. This is depending on the configuration that
+    // has been set for the event. Ticket graphs will be displayed before event graphs.
+
+    const dbInstance = db;
+
+    const graphs = await dbInstance.selectFrom(tEventsSalesConfiguration)
+        .where(tEventsSalesConfiguration.eventId.equals(eventId))
+            .and(tEventsSalesConfiguration.saleCategory.isNotNull())
+        .select({
+            category: tEventsSalesConfiguration.saleCategory,
+            categoryLimit: tEventsSalesConfiguration.saleCategoryLimit,
+            saleEventId: tEventsSalesConfiguration.saleEventId,
+            saleTypes:
+                dbInstance.aggregateAsArrayOfOneColumn(tEventsSalesConfiguration.eventSaleType),
+        })
+        .groupBy(tEventsSalesConfiguration.saleCategory, tEventsSalesConfiguration.saleEventId)
+        .executeSelectMany();
+
+    const eventGraphs: EventSalesGraphProps[] = [ /* no graphs */ ];
+    const ticketGraphs: TicketSalesGraphProps[] = [ /* no graphs */ ];
+
+    for (const graph of graphs) {
+        switch (graph.category) {
+            case kEventSalesCategory.Event:
+                eventGraphs.push({
+                    activityId: graph.saleEventId,
+                    category: graph.category,
+                    eventId: eventId,
+                    limit: graph.categoryLimit,
+                    products: graph.saleTypes,
+                    range,
+                });
+                break;
+
+            case kEventSalesCategory.Locker:
+                // TODO: Figure out what to do with lockers?
+                break;
+
+            case kEventSalesCategory.TicketFriday:
+            case kEventSalesCategory.TicketSaturday:
+            case kEventSalesCategory.TicketSunday:
+            case kEventSalesCategory.TicketWeekend:
+                ticketGraphs.push({
+                    category: graph.category,
+                    eventId: eventId,
+                    products: graph.saleTypes,
+                    range,
+                });
+                break;
+
+            default:
+                console.warn(`Unrecognised graph category: ${graph.category}`);
+                break;
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    return (
+        <Grid container spacing={2}>
+            { ticketGraphs.map((ticketGraphProps, index) =>
+                <Grid key={index} size={{ xs: 12, md: 6 }}>
+                    <Section noHeader>
+                        <Suspense fallback={ <LoadingGraph /> }>
+                            <TicketSalesGraph {...ticketGraphProps} />
+                        </Suspense>
+                    </Section>
+                </Grid> )}
+            { eventGraphs.map((eventGraphProps, index) =>
+                <Grid key={index} size={{ xs: 12, md: 6 }}>
+                    <Section noHeader>
+                        <Suspense fallback={ <LoadingGraph /> }>
+                            <EventSalesGraph {...eventGraphProps} />
+                        </Suspense>
+                    </Section>
+                </Grid> )}
+            { (!ticketGraphs.length && !eventGraphs.length) &&
+                <Grid size={12}>
+                    <Paper elevation={1} sx={{ p: 1.75 }}>
+                        <Alert severity="error" variant="outlined">
+                            No financial information is available, or has been configured for
+                            this event yet.
+                        </Alert>
+                    </Paper>
+                </Grid> }
+        </Grid>
+    );
+}
--- app/admin/events/[event]/finance/page.tsx
+++ app/admin/events/[event]/finance/page.tsx
@@ -1,12 +1,6 @@
 // Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import { Suspense } from 'react';
-
-import Alert from '@mui/material/Alert';
-import Grid from '@mui/material/Grid2';
-import Paper from '@mui/material/Paper';
-
 import type { NextPageParams } from '@lib/NextRouterParams';
 import { SalesConfigurationSection } from './SalesConfigurationSection';
 import { SalesUploadSection } from './SalesUploadSection';
@@ -15,13 +9,8 @@ import { SectionIntroduction } from '@app/admin/components/SectionIntroduction';
 import { generateEventMetadataFn } from '../generateEventMetadataFn';
 import { verifyAccessAndFetchPageInfo } from '@app/admin/events/verifyAccessAndFetchPageInfo';
 import { readUserSetting } from '@lib/UserSettings';
-import { selectRangeForEvent } from './graphs/SalesGraphUtils';
-import db, { tEventsSalesConfiguration } from '@lib/database';
 
-import { type EventSalesGraphProps, EventSalesGraph } from './graphs/EventSalesGraph';
-import { type TicketSalesGraphProps, TicketSalesGraph } from './graphs/TicketSalesGraph';
-import { LoadingGraph } from './graphs/LoadingGraph';
-import { kEventSalesCategory } from '@lib/database/Types';
+import { FinanceGraphGrid } from './FinanceGraphGrid';
 
 /**
  * The <EventFinancePage> page displays financial information of the event to those who have access
@@ -33,79 +22,17 @@ export default async function EventFinancePage(props: NextPageParams<'event'>) {
         permission: 'statistics.finances',
     });
 
-    const dbInstance = db;
-
     // Those who are allowed to manage an event's settings can manage financial information as well,
     // even though that does not necessarily mean that they can access the source data.
     const canManageFinances = access.can('event.settings', {
         event: event.slug,
     });
 
-    // ---------------------------------------------------------------------------------------------
-    // Determine the date ranges that should be displayed on the graphs. All graphs will show data
-    // along the same X axes to make sure that they're visually comparable.
-
-    const range = await selectRangeForEvent(event.id);
-
-    // ---------------------------------------------------------------------------------------------
-    // Determine that graphs that have to be displayed. This is depending on the configuration that
-    // has been set for the event. Ticket graphs will be displayed before event graphs.
-
-    const graphs = await dbInstance.selectFrom(tEventsSalesConfiguration)
-        .where(tEventsSalesConfiguration.eventId.equals(event.id))
-            .and(tEventsSalesConfiguration.saleCategory.isNotNull())
-        .select({
-            category: tEventsSalesConfiguration.saleCategory,
-            categoryLimit: tEventsSalesConfiguration.saleCategoryLimit,
-            saleEventId: tEventsSalesConfiguration.saleEventId,
-            saleTypes:
-                dbInstance.aggregateAsArrayOfOneColumn(tEventsSalesConfiguration.eventSaleType),
-        })
-        .groupBy(tEventsSalesConfiguration.saleCategory, tEventsSalesConfiguration.saleEventId)
-        .executeSelectMany();
-
-    const eventGraphs: EventSalesGraphProps[] = [ /* no graphs */ ];
-    const ticketGraphs: TicketSalesGraphProps[] = [ /* no graphs */ ];
-
-    for (const graph of graphs) {
-        switch (graph.category) {
-            case kEventSalesCategory.Event:
-                eventGraphs.push({
-                    activityId: graph.saleEventId,
-                    category: graph.category,
-                    eventId: event.id,
-                    limit: graph.categoryLimit,
-                    products: graph.saleTypes,
-                    range,
-                });
-                break;
-
-            case kEventSalesCategory.Locker:
-                // TODO: Figure out what to do with lockers?
-                break;
-
-            case kEventSalesCategory.TicketFriday:
-            case kEventSalesCategory.TicketSaturday:
-            case kEventSalesCategory.TicketSunday:
-            case kEventSalesCategory.TicketWeekend:
-                ticketGraphs.push({
-                    category: graph.category,
-                    eventId: event.id,
-                    products: graph.saleTypes,
-                    range,
-                });
-                break;
-
-            default:
-                console.warn(`Unrecognised graph category: ${graph.category}`);
-                break;
-        }
+    let configurationExpanded = false;
+    if (!!canManageFinances) {
+        configurationExpanded =
+            await readUserSetting(user.userId, 'user-admin-event-finance-configuration') ?? false;
     }
-
-    // ---------------------------------------------------------------------------------------------
-
-    const configurationExpanded =
-        await readUserSetting(user.userId, 'user-admin-event-finance-configuration') ?? false;
 
     return (
         <>
@@ -117,33 +44,7 @@ export default async function EventFinancePage(props: NextPageParams<'event'>) {
                     delayed.
                 </SectionIntroduction>
             </Section>
-            <Grid container spacing={2}>
-                { ticketGraphs.map((ticketGraphProps, index) =>
-                    <Grid key={index} size={{ xs: 12, md: 6 }}>
-                        <Section noHeader>
-                            <Suspense fallback={ <LoadingGraph /> }>
-                                <TicketSalesGraph {...ticketGraphProps} />
-                            </Suspense>
-                        </Section>
-                    </Grid> )}
-                { eventGraphs.map((eventGraphProps, index) =>
-                    <Grid key={index} size={{ xs: 12, md: 6 }}>
-                        <Section noHeader>
-                            <Suspense fallback={ <LoadingGraph /> }>
-                                <EventSalesGraph {...eventGraphProps} />
-                            </Suspense>
-                        </Section>
-                    </Grid> )}
-                { (!ticketGraphs.length && !eventGraphs.length) &&
-                    <Grid size={12}>
-                        <Paper elevation={1} sx={{ p: 1.75 }}>
-                            <Alert severity="error" variant="outlined">
-                                No financial information is available, or has been configured for
-                                this event yet.
-                            </Alert>
-                        </Paper>
-                    </Grid> }
-            </Grid>
+            <FinanceGraphGrid eventId={event.id} />
             { canManageFinances &&
                 <SalesConfigurationSection event={event.slug} expanded={configurationExpanded} /> }
             { canManageFinances &&
