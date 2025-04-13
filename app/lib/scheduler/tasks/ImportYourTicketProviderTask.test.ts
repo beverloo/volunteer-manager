07c8889a585a9543afb482963f2c8d7bd817810d
Peter Beverloo <pbeverloo@gmail.com>
2025-04-13 19:45:20 +0100

    feat: Teach `ImportYourTicketProviderTask` to configure its own interval

--- app/lib/scheduler/tasks/ImportYourTicketProviderTask.test.ts
+++ app/lib/scheduler/tasks/ImportYourTicketProviderTask.test.ts
@@ -1,17 +1,95 @@
 // Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
+import { ImportYourTicketProviderTask } from './ImportYourTicketProviderTask';
+import { TaskContext } from '../TaskContext';
+import { Temporal, formatDate } from '@lib/Temporal';
 import { useMockConnection } from '@lib/database/Connection';
 
 describe('ImportYourTicketProviderTask', () => {
     const mockConnection = useMockConnection();
 
+    interface FestivalOptions {
+        id: number;
+        name: string;
+        endTime: string;  // YYYY-MM-DD HH:mm:ss
+        yourTicketProviderId: number;
+    }
+
+    function createImportYourTicketProviderTask(
+        options?: FestivalOptions | FestivalOptions[],
+        skipUpdate?: boolean)
+    {
+        const context = TaskContext.forEphemeralTask('ImportYourTicketProviderTask', {});
+        const task = new class extends ImportYourTicketProviderTask {
+            constructor() {
+                super(context);
+            }
+
+            override async executeForEvent(dbInstance: any, event: any): Promise<void> {
+                if (!skipUpdate)
+                    await super.executeForEvent(dbInstance, event);
+            }
+        };
+
+        expect(task.isComplexTask()).toBeFalse();
+
+        mockConnection.expect('selectManyRows', () => {
+            if (!options)
+                return undefined;
+
+            return Array.isArray(options) ? options : [ options ];
+        });
+
+        return task;
+    }
+
     it('should skip when there are no upcoming festivals', async () => {
-        // TODO
+        const task = createImportYourTicketProviderTask([ /* no events */ ]);
+        expect(task.contextForTesting.intervalMsForTesting).toBeUndefined();
+
+        const result = await task.execute();
+        expect(result).toBeTrue();
+
+        expect(task.log.entries).toHaveLength(1);
+        expect(task.log.entries[0].message).toInclude('No future events');
+
+        expect(task.contextForTesting.intervalMsForTesting).toBe(
+            ImportYourTicketProviderTask.kIntervalMaximum);
     });
 
     it('should scale the task interval based on duration until the festival', async () => {
-        // TODO
+        const finalTestCase =
+            ImportYourTicketProviderTask.kIntervalConfiguration[
+                ImportYourTicketProviderTask.kIntervalConfiguration.length - 1];
+
+        const kTestCases = [
+            ...ImportYourTicketProviderTask.kIntervalConfiguration,
+            {
+                maximumDays: finalTestCase.maximumDays + 2,
+                intervalMs: ImportYourTicketProviderTask.kIntervalMaximum,
+            },
+        ];
+
+        expect(kTestCases.length).toBeGreaterThan(0);
+
+        for (const { maximumDays, intervalMs } of kTestCases) {
+            const task = createImportYourTicketProviderTask([
+                {
+                    id: 100,
+                    name: 'AnimeCon',
+                    endTime: formatDate(
+                        Temporal.Now.zonedDateTimeISO('UTC').add({ days: maximumDays }),
+                        'YYYY-MM-DD HH:mm:ss'),
+                    yourTicketProviderId: 1337,
+                }
+            ], /* skipUpdate= */ true);
+
+            const result = await task.execute();
+            expect(result).toBeTrue();
+
+            expect(task.contextForTesting.intervalMsForTesting).toBe(intervalMs);
+        }
     });
 
     it('should upsert information retrieved from the API in the database', async () => {
--- app/lib/scheduler/tasks/ImportYourTicketProviderTask.ts
+++ app/lib/scheduler/tasks/ImportYourTicketProviderTask.ts
@@ -2,6 +2,18 @@
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
 import { Task } from '../Task';
+import { Temporal } from '@lib/Temporal';
+import db, { tEvents } from '@lib/database';
+
+/**
+ * Information made available for a particular event for which YourTicketProvider should be queried.
+ */
+interface EventInfo {
+    id: number;
+    name: string;
+    endTime: Temporal.ZonedDateTime;
+    yourTicketProviderId: number;
+}
 
 /**
  * Task that imports ticket sales information from YourTicketProvider. The applicable event(s) will
@@ -9,7 +21,89 @@ import { Task } from '../Task';
  * be automatically altered based on proximity to the festival.
  */
 export class ImportYourTicketProviderTask extends Task {
+    /**
+     * The default interval for the import task, when no precise granularity can be decided upon.
+     */
+    static readonly kIntervalMaximum = /* 2 days= */ 2 * 24 * 3600 * 1000;
+
+    /**
+     * Intervals for the tasks based on the number of days until the event happens.
+     */
+    static readonly kIntervalConfiguration = [
+        { maximumDays: /* 4 weeks= */   28, intervalMs: /* 6 hours= */   6 * 3600 * 1000 },
+        { maximumDays: /* 8 weeks= */   56, intervalMs: /* 12 hours= */ 12 * 3600 * 1000 },
+        { maximumDays: /* 24 weeks= */ 168, intervalMs: /* 24 hours= */ 24 * 3600 * 1000 },
+    ];
+
+    /**
+     * Actually executes the task.
+     */
     override async execute(): Promise<boolean> {
+        const dbInstance = db;
+
+        const upcomingEvents = await this.determineApplicableUpcomingEvents(dbInstance);
+        if (!upcomingEvents.length) {
+            this.log.info('Interval: No future events with a ytpEventId, using maximum interval.');
+            this.setIntervalForRepeatingTask(ImportYourTicketProviderTask.kIntervalMaximum);
+            return true;
+        }
+
+        this.updateTaskIntervalForFestivalDate(upcomingEvents[0].endTime);
+
+        for (const event of upcomingEvents)
+            await this.executeForEvent(dbInstance, event);
+
         return true;
+    }
+
+    /**
+     * Actually executes the task for the given `event`.
+     */
+    async executeForEvent(dbInstance: typeof db, event: EventInfo): Promise<void> {
+        // TODO
+    }
+
+    /**
+     * Function that determines applicable upcoming events for importing data from the YTP API. This
+     * means that the event (1) must not have ended yet, and (2) must have an associated YTP Event
+     * ID for whihc information can be obtained.
+     */
+    async determineApplicableUpcomingEvents(dbInstance: typeof db): Promise<EventInfo[]> {
+        return await dbInstance.selectFrom(tEvents)
+            .where(tEvents.eventHidden.equals(/* false= */ 0))
+                .and(tEvents.eventEndTime.lessOrEquals(dbInstance.currentZonedDateTime()))
+                .and(tEvents.eventYtpId.isNotNull())
+            .select({
+                id: tEvents.eventId,
+                name: tEvents.eventShortName,
+                endTime: tEvents.eventEndTime,
+                yourTicketProviderId: tEvents.eventYtpId,
+            })
+            .orderBy(tEvents.eventEndTime, 'desc')
+            .executeSelectMany() as EventInfo[];
+    }
+
+    /**
+     * Updates the task's interval based on the distance between the current time and the given
+     * `endTime`, effectively applying the `kIntervalConfiguration`.
+     */
+    updateTaskIntervalForFestivalDate(endTime: Temporal.ZonedDateTime): void {
+        const differenceInDays = endTime.since(Temporal.Now.zonedDateTimeISO('UTC'), {
+            largestUnit: 'days',
+        }).days;
+
+        for (const configurationEntry of ImportYourTicketProviderTask.kIntervalConfiguration) {
+            const { maximumDays, intervalMs } = configurationEntry;
+
+            if (differenceInDays > maximumDays)
+                continue;
+
+            this.log.info(`Interval: Updating to ${intervalMs}ms (days=${differenceInDays})`);
+            this.setIntervalForRepeatingTask(intervalMs);
+            return;
+        }
+
+        this.log.info('Interval: The event is still very far out, using maximum interval.');
+        this.setIntervalForRepeatingTask(ImportYourTicketProviderTask.kIntervalMaximum);
     }
 }
