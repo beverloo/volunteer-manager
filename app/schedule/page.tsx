fa2e2c1da813e2fbd81b7400f7fe371866fcdd74
Peter Beverloo <pbeverloo@gmail.com>
2025-04-20 19:31:26 +0100

    feat: Redirect /schedule to the most recently available schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -3,10 +3,30 @@
 
 import { redirect } from 'next/navigation';
 
+import { getAuthenticationContext } from '@lib/auth/AuthenticationContext';
+import db, { tEvents } from '@lib/database';
+
 /**
- * The <EventlessSchedulePage> component redirects the user back to the homepage. Schedules can only
+ * The <EventlessSchedulePage> component redirects the user back either the schedule for the most
+ * recent event they're participating in, or the homepage when there is none. Schedules can only
  * be accessed for a specific event, so the event-less page is deliberately disabled.
  */
 export default async function EventlessSchedulePage() {
+    const authenticationContext = await getAuthenticationContext();
+    if (authenticationContext.user) {
+        const participatingEvents = [ ...authenticationContext.events.keys() ];
+
+        const eventSlug = await db.selectFrom(tEvents)
+            .where(tEvents.eventHidden.equals(/* false= */ 0))
+                .and(tEvents.eventSlug.in(participatingEvents))
+            .selectOneColumn(tEvents.eventSlug)
+            .orderBy(tEvents.eventStartTime, 'desc')
+            .limit(1)
+            .executeSelectNoneOrOne();
+
+        if (!!eventSlug)
+            redirect(`/schedule/${eventSlug}`);
+    }
+
     redirect('/');
 }
