95f20af921541b956f1fcecd7c09eaca4fc2ce77
Peter Beverloo <pbeverloo@gmail.com>
2025-05-11 13:08:00 +0100

    feat: Complete the Account Warning card on the Organisation dashboard

--- app/admin/organisation/dashboard/AccountWarningCard.tsx
+++ app/admin/organisation/dashboard/AccountWarningCard.tsx
@@ -1,7 +1,26 @@
 // Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import Paper from '@mui/material/Paper';
+import Link from 'next/link';
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
+import PhoneDisabledIcon from '@mui/icons-material/PhoneDisabled';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+
+import { DiscordIcon } from './DiscordIcon';
+import db, { tUsers } from '@lib/database';
+
+/**
+ * Regular expression to verify that phone numbers are stored in a E.164-compatible format.
+ */
+const kPhoneNumberRegex = /^\+[1-9]\d{1,14}$/;
 
 /**
  * The <AccountWarningCard> component is a card that visualises warnings related to accounts known
@@ -9,9 +28,90 @@ import Paper from '@mui/material/Paper';
  * handles, and so on.
  */
 export async function AccountWarningCard() {
+    const volunteers = await db.selectFrom(tUsers)
+        .select({
+            id: tUsers.userId,
+            name: tUsers.name,
+            phoneNumber: tUsers.phoneNumber,
+            discordHandle: tUsers.discordHandle,
+            discordHandleUpdated: tUsers.discordHandleUpdated,
+            activated: tUsers.activated.equals(/* true= */ 1),
+        })
+        .executeSelectMany();
+
+    // ---------------------------------------------------------------------------------------------
+    // Compute account warnings:
+    // ---------------------------------------------------------------------------------------------
+
+    type Warning = {
+        userId: number;
+        name: string;
+        priority: number;
+        icon: React.ReactNode;
+        text: string;
+    };
+
+    const warnings: Warning[] = [];
+
+    for (const volunteer of volunteers) {
+        if (!volunteer.activated) {
+            warnings.push({
+                userId: volunteer.id,
+                name: volunteer.name,
+                priority: 3,
+                icon: <PauseCircleOutlineIcon color="disabled" />,
+                text: 'Their account has not been activated yet',
+            })
+        }
+
+        if (!!volunteer.phoneNumber && !kPhoneNumberRegex.test(volunteer.phoneNumber)) {
+            warnings.push({
+                userId: volunteer.id,
+                name: volunteer.name,
+                priority: 2,
+                icon: <PhoneDisabledIcon color="info" />,
+                text: `Their phone number (${volunteer.phoneNumber}) is invalid`,
+            });
+        }
+
+        if (!!volunteer.discordHandle && !!volunteer.discordHandleUpdated) {
+            warnings.push({
+                userId: volunteer.id,
+                name: volunteer.name,
+                priority: 1,
+                icon: <DiscordIcon color="warning" />,
+                text: `Their Discord handle (${volunteer.discordHandle}) needs to be verified`,
+            });
+        }
+    }
+
+    warnings.sort((lhs, rhs) => {
+        if (lhs.priority !== rhs.priority)
+            return lhs.priority > rhs.priority ? 1 : -1;
+
+        return lhs.name.localeCompare(rhs.name);
+    });
+
+    // ---------------------------------------------------------------------------------------------
+
     return (
-        <Paper sx={{ p: 2 }}>
-            Account Warning Card
-        </Paper>
+        <Card>
+            <CardHeader avatar={ <WarningAmberIcon color="primary" /> }
+                        title="Account warnings"
+                        slotProps={{ title: { variant: 'subtitle2' } }} />
+            <Divider />
+                <List dense disablePadding>
+                    { warnings.map((warning, index) =>
+                        <ListItemButton LinkComponent={Link}
+                                        href={`/admin/organisation/accounts/${warning.userId}`}
+                                        key={index}>
+                            <ListItemIcon>
+                                {warning.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={warning.name}
+                                          secondary={warning.text} />
+                        </ListItemButton> )}
+                </List>
+        </Card>
     );
 }
