798edc367c1901d2f02cdbb69203295c249f8e04
Peter Beverloo <pbeverloo@gmail.com>
2025-05-11 12:58:46 +0100

    feat: Complete the Feedback card on the Organisation dashboard

--- app/admin/organisation/dashboard/FeedbackCard.tsx
+++ app/admin/organisation/dashboard/FeedbackCard.tsx
@@ -1,16 +1,89 @@
 // Copyright 2025 Peter Beverloo & AnimeCon. All rights reserved.
 // Use of this source code is governed by a MIT license that can be found in the LICENSE file.
 
-import Paper from '@mui/material/Paper';
+import React from 'react';
+import Link from 'next/link';
+
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Divider from '@mui/material/Divider';
+import FeedbackOutlinedIcon from '@mui/icons-material/FeedbackOutlined';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import { formatDate } from '@lib/Temporal';
+import db, { tFeedback, tUsers } from '@lib/database';
+
+import { kEnforceSingleLine } from '@app/schedule/[event]/Constants';
+
+/**
+ * Number of feedback entries that should be shown in the table.
+ */
+const kFeedbackEntries = 4;
 
 /**
  * The <FeedbackCard> component is a card that lists the most recently received feedback through the
  * Volunteer Portal. The card links through to the dedicated feedback page.
  */
 export async function FeedbackCard() {
+    const usersJoin = tUsers.forUseInLeftJoin();
+
+    const feedback = await db.selectFrom(tFeedback)
+        .leftJoin(usersJoin)
+            .on(usersJoin.userId.equals(tFeedback.userId))
+        .select({
+            id: tFeedback.feedbackId,
+            author: tFeedback.feedbackName.valueWhenNull(usersJoin.name),
+            date: tFeedback.feedbackDate,
+            feedback: tFeedback.feedbackText,
+        })
+        .orderBy(tFeedback.feedbackDate, 'desc')
+        .limit(kFeedbackEntries)
+        .executeSelectMany();
+
+    if (!feedback)
+        return <>{ /* no content */ }</>;
+
+    const completedFeedback = feedback.map(entry => ({
+        ...entry,
+        date: formatDate(entry.date, 'MMMM Do'),
+    }));
+
     return (
-        <Paper sx={{ p: 2 }}>
-            Feedback Card
-        </Paper>
+        <Card>
+            <CardHeader avatar={ <FeedbackOutlinedIcon color="primary" /> }
+                        title="Recent feedback"
+                        slotProps={{ title: { variant: 'subtitle2' } }} />
+            <Divider />
+            <CardContent sx={{ p: 0 }}>
+                <List dense disablePadding>
+                    { completedFeedback.map(entry =>
+                        <React.Fragment key={entry.id}>
+                            <ListItem>
+                                <ListItemIcon sx={{ minWidth: '40px' }}>
+                                    <LabelImportantIcon color="disabled" fontSize="small" />
+                                </ListItemIcon>
+                                <ListItemText primary={entry.feedback}
+                                            secondary={`— ${entry.author}, ${entry.date}`}
+                                            slotProps={{
+                                                primary: { sx: kEnforceSingleLine },
+                                            }}/>
+                            </ListItem>
+                            <Divider component="li" />
+                        </React.Fragment> )}
+                </List>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'flex-end' }}>
+                <Button LinkComponent={Link} href="/admin/organisation/feedback" size="small">
+                    Learn more
+                </Button>
+            </CardActions>
+        </Card>
     );
 }
